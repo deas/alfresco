d92f61a37542fddcd883edcbd197d8197cf70ca0
kevinr <kevinr@7033f55c-6c29-4ad8-afba-765cb616e738>
2011-09-27 10:52:11 +0000

    Fix for ALF-10005 - profile page toolbar did not handle all variations of the user profile page url templates Also fixed up following REST API to use readonly txns for GET methods.
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@30791 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/profile/userprofiletoolbar.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/profile/userprofiletoolbar.get.js
@@ -3,18 +3,25 @@
  */
 
 var userId = page.url.templateArgs["userid"];
-model.activeUserProfile = (userId == null || userId == user.name);
+if (userId == null)
+{
+   userId = user.name;
+}
+model.activeUserProfile = (userId == user.name);
 
 model.following = -1
 model.followers = -1
 
 var following = remote.call("/api/subscriptions/" + encodeURIComponent(userId) + "/following/count");
-if(following.status == 200) {
+if (following.status == 200)
+{
    model.following = eval('(' + following + ')').count;
-
-   if(model.activeUserProfile) {
+   
+   if (model.activeUserProfile)
+   {
 	   var followers = remote.call("/api/subscriptions/" + encodeURIComponent(userId) + "/followers/count");
-	   if(followers.status == 200) {
+	   if(followers.status == 200)
+	   {
 	      model.followers = eval('(' + followers + ')').count;
 	   }
    } 
