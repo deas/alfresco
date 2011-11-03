d7b2a9cb3a1e60e9942f6dfb8463fc5d1fe21598
kroast <kroast@7033f55c-6c29-4ad8-afba-765cb616e738>
2011-11-03 15:18:31 +0000

    Merged V3.4-BUG-FIX to HEAD    31640: Fix for ALF-9922 - Performance issue of Wiki-Dashlet page.
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@31686 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/dashlets/wiki.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/dashlets/wiki.get.js
@@ -1,35 +1,39 @@
 <import resource="classpath:alfresco/site-webscripts/org/alfresco/callutils.js">
 
-var wikipage = args.wikipage;
-if (wikipage)
+function main()
 {
-   var uri = "/slingshot/wiki/page/" + page.url.templateArgs.site + "/" + encodeURIComponent(wikipage) + "?format=mediawiki",
-      connector = remote.connect("alfresco"),
-      result = connector.get(uri),
-      myConfig = new XML(config.script);
-   
-   if (result.status == status.STATUS_OK)
+   var wikipage = args.wikipage;
+   if (wikipage)
    {
-      model.wikipage = myConfig.allowUnfilteredHTML == true ? result.response : stringUtils.stripUnsafeHTML(result.response);
+      var uri = "/slingshot/wiki/page/" + page.url.templateArgs.site + "/" + encodeURIComponent(wikipage) + "?format=mediawiki",
+          connector = remote.connect("alfresco"),
+          result = connector.get(uri),
+          myConfig = new XML(config.script);
+      
+      if (result.status == status.STATUS_OK)
+      {
+         model.wikipage = myConfig.allowUnfilteredHTML == true ? result.response : stringUtils.stripUnsafeHTML(result.response);
+      }
+      
+      model.wikiLink = String(wikipage);
+      model.pageTitle = String(wikipage).replace(/_/g, " ");
+   
+      // Get all pages for the site so we can display links correctly
+      model.pageList = doGetCall("/slingshot/wiki/pages/" + page.url.templateArgs.site + "?pageMetaOnly=true");
    }
    
-   model.wikiLink = String(wikipage);
-   model.pageTitle = String(wikipage).replace(/_/g, " ");
-
-   // Get all pages for the site so we can display links correctly
-   model.pageList = doGetCall("/slingshot/wiki/pages/" + page.url.templateArgs.site);
-}
-
-// Call the repository to see if the user is site manager or not
-var userIsSiteManager = false,
-   json = remote.call("/api/sites/" + page.url.templateArgs.site + "/memberships/" + encodeURIComponent(user.name));
-
-if (json.status == 200)
-{
-   var obj = eval('(' + json + ')');
-   if (obj)
+   // Call the repository to see if the user is site manager or not
+   var userIsSiteManager = false,
+       json = remote.call("/api/sites/" + page.url.templateArgs.site + "/memberships/" + encodeURIComponent(user.name));
+   
+   if (json.status == 200)
    {
-      userIsSiteManager = (obj.role == "SiteManager");
+      var obj = eval('(' + json + ')');
+      if (obj)
+      {
+         userIsSiteManager = (obj.role == "SiteManager");
+      }
    }
+   model.userIsSiteManager = userIsSiteManager;
 }
-model.userIsSiteManager = userIsSiteManager;
+main();
