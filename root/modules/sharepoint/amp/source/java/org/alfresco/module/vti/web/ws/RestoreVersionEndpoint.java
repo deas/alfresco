a02d159565dfc284275f2fc9dce11a14a4adf3cd
nburch <nburch@7033f55c-6c29-4ad8-afba-765cb616e738>
2011-11-03 00:43:52 +0000

    Refactor the restore versions SPP endpoint to follow the new pattern
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@31659 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/modules/sharepoint/amp/source/java/org/alfresco/module/vti/web/ws/RestoreVersionEndpoint.java
+++ root/modules/sharepoint/amp/source/java/org/alfresco/module/vti/web/ws/RestoreVersionEndpoint.java
@@ -35,115 +35,39 @@ import org.jaxen.dom4j.Dom4jXPath;
  *
  * @author PavelYur
  */
-public class RestoreVersionEndpoint extends AbstractEndpoint
+public class RestoreVersionEndpoint extends AbstractVersionEndpoint
 {
-
-    // handler that provides methods for operating with documents and folders
-    private VersionsServiceHandler handler;
-
-    // xml namespace prefix
-    private static String prefix = "versions";
-
     private static Log logger = LogFactory.getLog(RestoreVersionEndpoint.class);
-    
+
     public RestoreVersionEndpoint(VersionsServiceHandler handler)
     {
-        this.handler = handler;
+        super(handler);
+    }
+    
+    
+    @Override
+    protected String getEndpointNamePrefix() 
+    {
+       return "RestoreVersion";
     }
 
-    /**
-     * Restore specified version of the document, makes it current working version
-     * 
-     * @param soapRequest Vti soap request ({@link VtiSoapRequest})
-     * @param soapResponse Vti soap response ({@link VtiSoapResponse}) 
-     */
-    public void execute(VtiSoapRequest soapRequest, VtiSoapResponse soapResponse) throws Exception
-    {
+    @Override
+   protected List<DocumentVersionBean> executeVersionAction(
+         VtiSoapRequest soapRequest, String dws, String fileName, Element fileVersion) throws Exception 
+   {
         if (logger.isDebugEnabled())
-            logger.debug("Soap Method with name " + getName() + " is started.");
-        // mapping xml namespace to prefix
-        SimpleNamespaceContext nc = new SimpleNamespaceContext();
-        nc.addNamespace(prefix, namespace);
-        nc.addNamespace(soapUriPrefix, soapUri);
-        
-        String host = getHost(soapRequest);
-        String context = soapRequest.getAlfrescoContextName();
-        String dws = getDwsFromUri(soapRequest);
-
-        Element element = soapRequest.getDocument().getRootElement();
-        
-        if (logger.isDebugEnabled())
-            logger.debug("Getting fileName parameter from request.");
-        XPath fileNameXPath = new Dom4jXPath(buildXPath(prefix, "/RestoreVersion/fileName"));
-        fileNameXPath.setNamespaceContext(nc);
-        Element fileName = (Element) fileNameXPath.selectSingleNode(element);
-
-        if (logger.isDebugEnabled())
-            logger.debug("Getting fileVersion parameter from request.");
-        XPath fileVersionXPath = new Dom4jXPath(buildXPath(prefix, "/RestoreVersion/fileVersion"));
-        fileVersionXPath.setNamespaceContext(nc);
-        Element fileVersion = (Element) fileVersionXPath.selectSingleNode(element);
-
-        if (logger.isDebugEnabled())
-            logger.debug("Restoring version " + fileVersion.getText() + " for file '" + dws + "/" + fileName.getText() + "'" );
-        List<DocumentVersionBean> notSortedVersions = handler.restoreVersion(dws + "/" + fileName.getText(), fileVersion.getText());
-        
-        // creating soap response
-        Element root = soapResponse.getDocument().addElement("RestoreVersionResponse", namespace);
-        Element restoreVersionResult = root.addElement("RestoreVersionResult");
-
-        Element results = restoreVersionResult.addElement("results", namespace);
-
-        results.addElement("list").addAttribute("id", "");
-        results.addElement("versioning").addAttribute("enabled", "1");
-        results.addElement("settings").addAttribute("url", host + context + dws + "/documentDetails.vti?doc=" + dws + "/" + fileName.getText());
-
-
+            logger.debug("Restoring version " + fileVersion.getText() + " for file '" + dws + "/" + fileName + "'" );
+        List<DocumentVersionBean> notSortedVersions = handler.restoreVersion(dws + "/" + fileName, fileVersion.getText());
+ 
+        // Sort as required
         List<DocumentVersionBean> versions = new ArrayList<DocumentVersionBean>();
         
         versions.add(notSortedVersions.get(0));
         for (int i = notSortedVersions.size() - 1; i > 0; --i) {
             versions.add(notSortedVersions.get(i));
         }
-        
-        boolean isCurrent = true;
-        for (DocumentVersionBean version : versions)
-        {
-            Element result = results.addElement("result");
-            if (isCurrent)
-            {
-                // prefix @ means that it is current working version, it couldn't be restored or deleted
-                result.addAttribute("version", "@" + version.getVersion());
-                String url = host + context + dws + "/" + fileName.getTextTrim();
-                result.addAttribute("url", url);
-                isCurrent = false;
-            }
-            else
-            {
-                result.addAttribute("version", version.getVersion());
-                String url = host + context + dws + version.getUrl();
-                result.addAttribute("url", url);
-            }
-            
-            result.addAttribute("created", version.getCreatedTime());
-            result.addAttribute("createdBy", version.getCreatedBy());
-            result.addAttribute("size", String.valueOf(version.getSize()));
-            result.addAttribute("comments", version.getComments());
-        }
-        
-        if (logger.isDebugEnabled()) {
-            String versionsStr = "";
-            for (DocumentVersionBean version : versions)
-            {
-                versionsStr += version.getVersion() + " ";
-            }
-            logger.debug("Now document has the folloving versions [ "+ versionsStr + "]");
-        }   
-        
-        if (logger.isDebugEnabled())
-        {
-            logger.debug("Soap Method with name " + getName() + " is finished.");
-        }
-    }
 
+        // All done
+        return versions;
+    }
 }
