af5a559ade2d8c8c424efb00ae353ac814223f32
rwetherall <rwetherall@7033f55c-6c29-4ad8-afba-765cb616e738>
2011-11-30 01:37:19 +0000

    MediaWiki Integration removed
     * Can now be found at http://code.google.com/p/alfresco-php-sdk/
    
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@32406 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/modules/mediawiki-integration/config/alfresco/module/org.alfresco.module.mediawikiintegration/template/mediawiki_site_page.php
+++
@@ -1,5 +0,0 @@
-<?php $site = $_ALF_MODEL["site"]; ?>
-
-==Welcome to the home page of <?php echo $site->cm_title ?>==
-
-<?php echo $site->cm_description ?>
--- root/modules/mediawiki-integration/source/php/mediawiki/maintenance/getSlaveServer.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once( dirname(__FILE__).'/commandLine.inc' );
-$i = $wgLoadBalancer->getReaderIndex();
-print $wgDBservers[$i]['host'] . "\n";
-
-?>
