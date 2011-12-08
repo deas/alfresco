3b88c4bb8b2aaf7539c9bc5d604e25587d824b5c
ewinlof <ewinlof@7033f55c-6c29-4ad8-afba-765cb616e738>
2011-12-08 16:13:17 +0000

    Fixed ALF-11627 "Collaborator could not revert previous version"
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@32649 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/document-details/document-versions.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/document-details/document-versions.get.js
@@ -8,7 +8,8 @@ function main()
    var documentDetails = AlfrescoUtil.getNodeDetails(model.nodeRef, model.site);
    if (documentDetails)
    {
-      model.allowNewVersionUpload = documentDetails.item.node.permissions.user["Write"] || false;
+      var userPermissions = documentDetails.item.node.permissions;
+      model.allowNewVersionUpload = (userPermissions["Write"] && userPermissions["Delete"]) || false;
       if (documentDetails.workingCopy && documentDetails.workingCopy.workingCopyVersion)
       {
          model.workingCopyVersion = documentDetails.workingCopy.workingCopyVersion;
