26c8354fa4908c99f695b0cb23bdfa9a08a575b7
mhatfield <mhatfield@7033f55c-6c29-4ad8-afba-765cb616e738>
2011-12-08 15:00:05 +0000

    Fix ALF-11895 "After upgrade to 4.0.c, cannot open existing folder with "#" symbols in the folder name"
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@32642 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/documentlibrary/data/surf-doclist.lib.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/documentlibrary/data/surf-doclist.lib.js
@@ -809,3 +809,45 @@ var DocList =
       }
    }
 };
+
+var surfDoclist_main = function surfDoclist_main()
+{
+   var json = "{}",
+      webscript = url.templateArgs.webscript,
+      params = encodeURIComponent(url.templateArgs.params).replace(/%2f/ig,"/"),
+      dataUrl = "/slingshot/doclib2/" + webscript + "/" + params,
+      argsArray = [];
+
+   // Need to reconstruct and encode original args
+   if (args.length > 0)
+   {
+      for (arg in args)
+      {
+         argsArray.push(arg + "=" + encodeURIComponent(args[arg].replace(/%25/g,"%2525")));
+      }
+      
+      dataUrl += "?" + argsArray.join("&");
+   }
+   
+   var result = remote.call(dataUrl);
+   if (result.status == 200)
+   {
+      var obj = eval('(' + result + ')');
+      if (obj && (obj.item || obj.items))
+      {
+         DocList.processResult(obj,
+         {
+            actions: true,
+            indicators: true,
+            metadataTemplate: true
+         });
+         json = jsonUtils.toJSONString(obj);
+      }
+   }
+   else
+   {
+      status.setCode(result.status);
+   }
+
+   model.json = json;
+};
