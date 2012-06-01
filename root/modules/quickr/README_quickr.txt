4aee771dcc749a01775fa82f32568a015f5a073e
dward <dward@7033f55c-6c29-4ad8-afba-765cb616e738>
2012-06-01 11:22:08 +0000

    ALF-14381: Remove source for Quickr Connector
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@37351 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/modules/quickr/README_quickr.txt
+++
@@ -1,3 +0,0 @@
-Quickr Integration Module
--------------------------
-
--- root/modules/quickr/source/generated/org/alfresco/repo/lotus/ws/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservices.clb.content.ibm.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.alfresco.repo.lotus.ws;
