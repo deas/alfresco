4fe69f1fab43ca63da53e042bef68f7335bfecbf
ddraper <ddraper@7033f55c-6c29-4ad8-afba-765cb616e738>
2012-07-13 10:48:38 +0000

    ALF-15228: Fixed people-finder refactor error
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@39244 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/people-finder/people-finder.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/people-finder/people-finder.get.js
@@ -15,7 +15,7 @@ function main()
          maxSearchResults : (args.maxSearchResults != null) ? args.maxSearchResults : defaultMaxSearchResults,
          setFocus : (args.setFocus != null) ? args.setFocus : "false",
          addButtonSuffix : (args.addButtonSuffix != null) ? args.addButtonSuffix : "",
-         dataWebScript : ((args.dataWebScript != null) ? args.dataWebScript : "api/groups").replace(/{/g, "[").replace(/}/g, "]"),
+         dataWebScript : ((args.dataWebScript != null) ? args.dataWebScript : "api/people").replace("[", "{").replace("]", "}"),
          viewMode : { ___value : "Alfresco.PeopleFinder.VIEW_MODE_DEFAULT", ___type: "REFERENCE"}
       }
    };
