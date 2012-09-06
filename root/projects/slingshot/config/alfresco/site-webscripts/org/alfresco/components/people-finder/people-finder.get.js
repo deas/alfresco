b6a7b0300d6a042845c787395aba8b28af8284f0
kroast <kroast@7033f55c-6c29-4ad8-afba-765cb616e738>
2012-09-06 13:42:30 +0000

    Fix for ALF-15443 "Can't add user to group" and ALF-15817 "The invite button has been replaced by a follow button when inviting users to a site." - minor refactoring issue with people finder component.
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@41324 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/people-finder/people-finder.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/people-finder/people-finder.get.js
@@ -6,21 +6,20 @@ function main()
        defaultMaxSearchResults = searchConfig.getChildValue('max-search-results');
 
    var peopleFinder = {
-      id : "PeopleFinder", 
-      name : "Alfresco.PeopleFinder",
-      options : {
-         userId : user.name,
-         siteId : (this.page != null) ? ((this.page.url.templateArgs.site != null) ? this.page.url.templateArgs.site : "") : ((args.site != null) ? args.site : ""),
-         minSearchTermLength : parseInt((args.minSearchTermLength != null) ? args.minSearchTermLength : defaultMinSearchTermLength),
-         maxSearchResults : parseInt((args.maxSearchResults != null) ? args.maxSearchResults : defaultMaxSearchResults),
-         setFocus : Boolean((args.setFocus != null) ? args.setFocus : "false"),
-         addButtonSuffix : (args.addButtonSuffix != null) ? args.addButtonSuffix : "",
-         dataWebScript : ((args.dataWebScript != null) ? args.dataWebScript : "api/people").replace("[", "{").replace("]", "}"),
-         viewMode : { _alfValue : "Alfresco.PeopleFinder.VIEW_MODE_FULLPAGE", _alfType: "REFERENCE"}
+      id: "PeopleFinder", 
+      name: "Alfresco.PeopleFinder",
+      options: {
+         userId: user.name,
+         siteId: (this.page != null) ? ((this.page.url.templateArgs.site != null) ? this.page.url.templateArgs.site : "") : ((args.site != null) ? args.site : ""),
+         minSearchTermLength: parseInt((args.minSearchTermLength != null) ? args.minSearchTermLength : defaultMinSearchTermLength),
+         maxSearchResults: parseInt((args.maxSearchResults != null) ? args.maxSearchResults : defaultMaxSearchResults),
+         setFocus: Boolean((args.setFocus != null) ? args.setFocus : "false"),
+         addButtonSuffix: (args.addButtonSuffix != null) ? args.addButtonSuffix : "",
+         dataWebScript: ((args.dataWebScript != null) ? args.dataWebScript : "api/people").replace("[", "{").replace("]", "}"),
+         viewMode: { _alfValue : args.viewMode != null ? args.viewMode : "Alfresco.PeopleFinder.VIEW_MODE_DEFAULT", _alfType: "REFERENCE"}
       }
    };
    model.widgets = [peopleFinder];
 }
 
 main();
-
