d611335cb5c2fabc49be6ce4bfea6cbee3c23ab3
ddraper <ddraper@7033f55c-6c29-4ad8-afba-765cb616e738>
2012-10-23 11:42:27 +0000

    ALF-16468: Ensure that created content navigation goes to document-details and not folder-details page
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@42984 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/create-content/create-content-mgr.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/create-content/create-content-mgr.get.js
@@ -5,7 +5,7 @@ function main() {
       name : "Alfresco.CreateContentMgr",
       options : {
          siteId : (page.url.templateArgs.site != null) ? page.url.templateArgs.site : "",
-         isContainer: Boolean((page.url.args.isContainer != null) ? page.url.args.isContainer : "false")
+         isContainer: "true" == ((page.url.args.isContainer != null) ? page.url.args.isContainer : "false")
       }
    };
    model.widgets = [widget];
