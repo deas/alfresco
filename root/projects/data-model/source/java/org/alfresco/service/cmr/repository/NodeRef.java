75947272eebcf5ee2dffd9b1b24c12bfbbe1ca2f
ragauss <ragauss@7033f55c-6c29-4ad8-afba-765cb616e738>
2012-11-02 11:53:52 +0000

    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@43335 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/data-model/source/java/org/alfresco/service/cmr/repository/NodeRef.java
+++ root/projects/data-model/source/java/org/alfresco/service/cmr/repository/NodeRef.java
@@ -102,7 +102,7 @@ public final class NodeRef implements EntityRef, Serializable
         int lastForwardSlash = nodeRef.lastIndexOf('/');
         if(lastForwardSlash == -1)
         {
-            throw new AlfrescoRuntimeException("Invalid node ref - does not contain forward slash: " + nodeRef);
+            throw new MalformedNodeRefException("Invalid node ref - does not contain forward slash: " + nodeRef);
         }
         this.storeRef = new StoreRef(nodeRef.substring(0, lastForwardSlash));
         this.id = nodeRef.substring(lastForwardSlash+1);
