deas/alfresco

commit e88ef3be5420720ec65eb1d39a6e9aee83ecd8f0

Merged THOR1_SPRINTS to HEAD    Performance improvement: prevent unnecessary 304 revalidation requests for thumbnails in detailed view of My-Documents and Recently Modified Documents dashlets    Fixed bean config problem (caused by r34662)    Fix build break    Refactored revalidation code to remove previously added WebScripts that are now surplus to requirements    Performance improvement: prevent unnecessary 304 revalidation for avatars on site colleagues dashlet    Performance improvement: prevent unnecessary 304 revalidation for avatars on following/follwers pages    Performance improvement: prevent unnecessary 304 revalidation for avatars in activity feeds    Performance improvement: prevent unecessary 304 revalidation for user avatar thumbnails in header WebScript    Prevent 304 revalidations for unchanged thumbnails in document library, web preview and search
git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@34698 7033f55c-6c29-4ad8-afba-765cb616e738

root/projects/repository/source/java/org/alfresco/repo/activities/ActivityServiceImpl.java
@@ -20,10 +20,14 @@ package org.alfresco.repo.activities;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.alfresco.error.AlfrescoRuntimeException;
+import org.alfresco.model.ContentModel;
 import org.alfresco.repo.activities.feed.cleanup.FeedCleaner;
 import org.alfresco.repo.domain.activities.ActivityFeedDAO;
 import org.alfresco.repo.domain.activities.ActivityFeedEntity;
@@ -35,11 +39,16 @@ import org.alfresco.repo.tenant.TenantService;
 import org.alfresco.service.cmr.activities.ActivityPostService;
 import org.alfresco.service.cmr.activities.ActivityService;
 import org.alfresco.service.cmr.activities.FeedControl;
+import org.alfresco.service.cmr.repository.AssociationRef;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.security.AuthorityService;
+import org.alfresco.service.cmr.security.PersonService;
 import org.alfresco.service.cmr.site.SiteInfo;
 import org.alfresco.service.cmr.site.SiteService;
 import org.alfresco.service.namespace.QName;
+import org.alfresco.service.namespace.RegexQNamePattern;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONException;
@@ -62,6 +71,8 @@ public class ActivityServiceImpl implements ActivityService, InitializingBean
     private TenantService tenantService;
     private SiteService siteService;
     private ActivityPostService activityPostService;
+    private PersonService personService;
+    private NodeService nodeService;
     
     private int maxFeedItems = 100;
     
@@ -110,6 +121,16 @@ public class ActivityServiceImpl implements ActivityService, InitializingBean
     public void setActivityPostService(ActivityPostService activityPostService)
     {
         this.activityPostService = activityPostService;
+    }
+    
+    public void setPersonService(PersonService personService)
+    {
+        this.personService = personService;
+    }
+    
+    public void setNodeService(NodeService nodeService)
+    {
+        this.nodeService = nodeService;
     }
     
     
@@ -253,6 +274,12 @@ public class ActivityServiceImpl implements ActivityService, InitializingBean
             }
             
             List<ActivityFeedEntity> activityFeeds = feedDAO.selectUserFeedEntries(feedUserId, format, siteId, excludeThisUser, excludeOtherUsers, minFeedId, maxFeedItems);
+
+            // Create a local cache just for this method to map IDs of users to their avatar NodeRef. This
+            // is local to the method because we only want to cache per request - there is not point in keeping
+            // an instance cache because the data will become stale if a user changes their avatar.
+            Map<String, NodeRef> userIdToAvatarNodeRefCache = new HashMap<String, NodeRef>();
+            
             
             for (ActivityFeedEntity activityFeed : activityFeeds)
             {
@@ -262,6 +289,36 @@ public class ActivityServiceImpl implements ActivityService, InitializingBean
                 
                 if (userFilter != null && !userFilter.contains(activityFeed.getPostUserId())) {
                     continue;
+                }
+                
+                // In order to prevent unnecessary 304 revalidations on user avatars in the activity stream the 
+                // activity posting user avatars will be retrieved and added to the activity feed. This will enable
+                // avatars to be requested using the unique nodeRef which can be safely cached by the browser and
+                // improve performance...
+                if (userIdToAvatarNodeRefCache.containsKey(activityFeed.getPostUserId()))
+                {
+                    // If we've previously cached the users avatar, or if we've determine that the user doesn't
+                    // have an avatar then use the cached data.
+                    activityFeed.setPostUserAvatarNodeRef(userIdToAvatarNodeRefCache.get(activityFeed.getPostUserId()));
+                }
+                else
+                {
+                    // Get the avatar for the user id, set it in the activity feed and update the cache
+                    NodeRef avatarNodeRef = null;
+                    NodeRef postPerson = this.personService.getPerson(activityFeed.getPostUserId());
+                    List<AssociationRef> assocRefs = this.nodeService.getTargetAssocs(postPerson, ContentModel.ASSOC_AVATAR);
+                    if (!assocRefs.isEmpty())
+                    {
+                        avatarNodeRef = assocRefs.get(0).getTargetRef();
+                        activityFeed.setPostUserAvatarNodeRef(avatarNodeRef);
+                    }
+                    else
+                    {
+                        activityFeed.setPostUserAvatarNodeRef(null);
+                    }
+                    
+                    // Update the cache (setting null if there is no avatar for the user)...
+                    userIdToAvatarNodeRefCache.put(activityFeed.getPostUserId(), avatarNodeRef);
                 }
                 
                 activityFeed.setSiteNetwork(tenantService.getBaseName(activityFeed.getSiteNetwork()));

root/projects/data-model/source/java/org/alfresco/model/ContentModel.java
@@ -320,6 +320,10 @@ public interface ContentModel
     static final QName PROP_CONTENT_PROPERTY_NAME = QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, "contentPropertyName");
     static final QName PROP_AUTOMATIC_UPDATE = QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, "automaticUpdate");
     
+    // Thumbnail modification handling
+    public static final QName ASPECT_THUMBNAIL_MODIFICATION = QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, "thumbnailModification");
+    public static final QName PROP_LAST_THUMBNAIL_MODIFICATION_DATA = QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, "lastThumbnailModification"); 
+    
     // The below content entities can be used to manage 'failed' thumbnails. These are thumbnails that execute and fail with an
     // exception that likely means a reattempt will fail. The failedThumbnailSource aspect can be used to mark a node as
     // having tried and failed to use a particular thumbnail definition. This can then be checked and reattempts at that thumbnail

root/projects/remote-api/config/alfresco/templates/webscripts/org/alfresco/repository/site/membership/membership.lib.ftl
@@ -24,7 +24,9 @@
 		"firstName": "${authority.properties.firstName!""}",
 		"lastName": "${authority.properties.lastName!""}",
 	<#if authority.assocs["cm:avatar"]??>
-		"avatar": "${"api/node/" + authority.assocs["cm:avatar"][0].nodeRef?string?replace('://','/') + "/content/thumbnails/avatar"}",
+	   <#assign avatarNodeRef>${authority.assocs["cm:avatar"][0].nodeRef?string?replace('://','/')}</#assign>
+		"avatar": "${"api/node/" + avatarNodeRef + "/content/thumbnails/avatar"}",
+		"avatarNode": "${avatarNodeRef}",
 	</#if>
 	<#if authority.properties.jobtitle??>
 		"jobtitle": "${authority.properties.jobtitle}",

root/projects/remote-api/config/alfresco/templates/webscripts/org/alfresco/repository/thumbnail/thumbnail.get.desc.xml
@@ -7,6 +7,7 @@
   Please note that Alfresco does not currently use the filename template-arg and that it will be ignored.
   Therefore a GET to these URLs will return the same resource as to the equivalent URLs without it.
   </description>
+  <url>/api/node/{store_type}/{store_id}/{id}/content{property}/thumbnails/{thumbnailname}?c={queueforcecreate?}&amp;ph={placeholder?}&amp;lastModified={modified?}</url>
   <url>/api/node/{store_type}/{store_id}/{id}/content{property}/thumbnails/{thumbnailname}?c={queueforcecreate?}&amp;ph={placeholder?}</url>  
   <url>/api/path/{store_type}/{store_id}/{id}/content{property}/thumbnails/{thumbnailname}?c={queueforcecreate?}&amp;ph={placeholder?}</url>
   <url>/api/node/{store_type}/{store_id}/{id}/content{property}/thumbnails/{thumbnailname}/{filename}?c={queueforcecreate?}&amp;ph={placeholder?}</url>  

root/projects/remote-api/config/alfresco/templates/webscripts/org/alfresco/repository/thumbnail/thumbnail.get.js
@@ -1,5 +1,17 @@
 function main()
 {
+   // Indicate whether or not the thumbnail can be cached by the browser. Caching is allowed if the lastModified
+   // argument is provided as this is an indication of request uniqueness and therefore the browser will have
+   // the latest thumbnail image.
+   if (args.lastModified != null)
+   {
+      model.allowBrowserToCache = "true";
+   }
+   else
+   {
+      model.allowBrowserToCache = "false";
+   }
+   
    // Get the node from the URL
    var pathSegments = url.match.split("/");
    var reference = [ url.templateArgs.store_type, url.templateArgs.store_id ].concat(url.templateArgs.id.split("/"));

root/projects/remote-api/config/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary/item.lib.ftl
@@ -47,6 +47,13 @@
    "modifiedOn": "<@dateFormat node.properties.modified />",
    "modifiedBy": "${modifiedBy}",
    "modifiedByUser": "${modifiedByUser}",
+   <#if node.hasAspect("cm:thumbnailModification")>
+      <#list node.properties.lastThumbnailModification as thumbnailMod>
+         <#if thumbnailMod?contains("doclib")>
+   "lastThumbnailModification": "${thumbnailMod}",
+         </#if>
+      </#list>
+   </#if>
    "lockedBy": "${lockedBy}",
    "lockedByUser": "${lockedByUser}",
    "size": "${node.size?c}",

root/projects/remote-api/config/alfresco/templates/webscripts/org/alfresco/slingshot/profile/avatar.get.desc.xml
@@ -3,6 +3,10 @@
    <description>
    Returns a user avatar image in the format specified by the thumbnailname, or the "avatar" preset if omitted.
    </description>
+   <url>/slingshot/profile/avatar/avatar</url>
+   <url>/slingshot/profile/avatar/avatar/thumbnail/{thumbnailname}</url>
+   <url>/slingshot/profile/avatar/{store_type}/{store_id}/{id}/thumbnail/{thumbnailname}</url>
+   <url>/slingshot/profile/avatar/{store_type}/{store_id}/{id}</url>
    <url>/slingshot/profile/avatar/{username}/thumbnail/{thumbnailname}</url>
    <url>/slingshot/profile/avatar/{username}</url>
    <format default="">argument</format>

root/projects/remote-api/config/alfresco/templates/webscripts/org/alfresco/slingshot/profile/avatar.get.js
@@ -23,49 +23,78 @@ function getPlaceholder(thumbnailName)
 function main()
 {
    var userName = url.templateArgs.username,
-      thumbnailName = url.templateArgs.thumbnailname || "avatar",
-      person = people.getPerson(userName);
+       thumbnailName = url.templateArgs.thumbnailname || "avatar",
+       avatarNode;
    
-   if (person == null)
+   // If there is no store type, store id or id on the request then this WebScript has most likely been requested
+   // for a user with no avatar image so we will just return the placeholder image.
+   if (userName == null && url.templateArgs.store_type == null && url.templateArgs.store_id == null && url.templateArgs.id == null)
    {
-      // Stream the placeholder image
+      // If there is no userName or nodeRef data then we want to return the browser cacheable placeholder...
       model.contentPath = getPlaceholder(thumbnailName);
+      model.allowBrowserToCache = "true";
       return;
    }
-   
-   // Retrieve the avatar NodeRef for this person, if there is one.
-   var avatarAssoc = person.assocs["cm:avatar"];
-   
-   if (avatarAssoc != null)
+   else if (url.templateArgs.store_type == null && url.templateArgs.store_id == null && url.templateArgs.id == null)
    {
-      var avatarNode = avatarAssoc[0];
-      if (avatarNode != null)
+      // There is no nodeRef data but there is a username... this should return the user image that needs revalidation
+      var person = people.getPerson(userName);
+      if (person == null)
       {
-         // Get the thumbnail
-         var thumbnail = avatarNode.getThumbnail(thumbnailName);
-         if (thumbnail == null || thumbnail.size == 0)
+         // Stream the placeholder image
+         model.contentPath = getPlaceholder(thumbnailName);
+         return;
+      }
+      else
+      {
+         // Retrieve the avatar NodeRef for this person, if there is one.
+         var avatarAssoc = person.assocs["cm:avatar"];
+         if (avatarAssoc != null)
          {
-            // Remove broken thumbnail
-            if (thumbnail != null)
-            {
-               thumbnail.remove();
-            }
-            
-            // Force the creation of the thumbnail
-            thumbnail = avatarNode.createThumbnail(thumbnailName, false);
-            if (thumbnail != null)
-            {
-               model.contentNode = thumbnail;
-               return;
-            }
+            avatarNode = avatarAssoc[0];
          }
-         else
+      }
+   }
+   else if (userName == null)
+   {
+      // There is no user name but there is nodeREf data... this should return the image that CAN be cached by the browser
+      model.allowBrowserToCache = "true";
+      avatarNode = search.findNode(url.templateArgs.store_type + "://" + url.templateArgs.store_id + "/" + url.templateArgs.id);
+      if (avatarNode == null)
+      {
+         // Stream the placeholder image if the avatar node cannot be found.
+         model.contentPath = getPlaceholder(thumbnailName);
+         return;
+      }
+   }
+   
+   // Get the thumbnail for the avatar...
+   if (avatarNode != null)
+   {
+      // Get the thumbnail
+      var thumbnail = avatarNode.getThumbnail(thumbnailName);
+      if (thumbnail == null || thumbnail.size == 0)
+      {
+         // Remove broken thumbnail
+         if (thumbnail != null)
          {
-            // Place the details of the thumbnail into the model, this will be used to stream the content to the client
+            thumbnail.remove();
+         }
+         
+         // Force the creation of the thumbnail
+         thumbnail = avatarNode.createThumbnail(thumbnailName, false);
+         if (thumbnail != null)
+         {
             model.contentNode = thumbnail;
             return;
-         } 
+         }
       }
+      else
+      {
+         // Place the details of the thumbnail into the model, this will be used to stream the content to the client
+         model.contentNode = thumbnail;
+         return;
+      } 
    }
    
    // Stream the placeholder image

commit 27c4c2f1301a92747c62062e25530c5088c129a3

ALF-16669: removing a site member may break the activity feed - fallout from r34698 - perf' improvement to prevent unnecessary 304 revalidation requests
git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@43449 7033f55c-6c29-4ad8-afba-765cb616e738

root/projects/repository/source/java/org/alfresco/repo/activities/ActivityServiceImpl.java
@@ -21,7 +21,6 @@ package org.alfresco.repo.activities;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -40,15 +39,14 @@ import org.alfresco.service.cmr.activities.ActivityPostService;
 import org.alfresco.service.cmr.activities.ActivityService;
 import org.alfresco.service.cmr.activities.FeedControl;
 import org.alfresco.service.cmr.repository.AssociationRef;
-import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.security.AuthorityService;
+import org.alfresco.service.cmr.security.NoSuchPersonException;
 import org.alfresco.service.cmr.security.PersonService;
 import org.alfresco.service.cmr.site.SiteInfo;
 import org.alfresco.service.cmr.site.SiteService;
 import org.alfresco.service.namespace.QName;
-import org.alfresco.service.namespace.RegexQNamePattern;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONException;
@@ -304,31 +302,39 @@ public class ActivityServiceImpl implements ActivityService, InitializingBean
                 // activity posting user avatars will be retrieved and added to the activity feed. This will enable
                 // avatars to be requested using the unique nodeRef which can be safely cached by the browser and
                 // improve performance...
-                if (userIdToAvatarNodeRefCache.containsKey(activityFeed.getPostUserId()))
+                NodeRef avatarNodeRef = null;
+                String postUserId = activityFeed.getPostUserId();
+                if (userIdToAvatarNodeRefCache.containsKey(postUserId))
                 {
                     // If we've previously cached the users avatar, or if we've determine that the user doesn't
                     // have an avatar then use the cached data.
-                    activityFeed.setPostUserAvatarNodeRef(userIdToAvatarNodeRefCache.get(activityFeed.getPostUserId()));
+                    avatarNodeRef = userIdToAvatarNodeRefCache.get(postUserId);
                 }
                 else
                 {
-                    // Get the avatar for the user id, set it in the activity feed and update the cache
-                    NodeRef avatarNodeRef = null;
-                    NodeRef postPerson = this.personService.getPerson(activityFeed.getPostUserId());
-                    List<AssociationRef> assocRefs = this.nodeService.getTargetAssocs(postPerson, ContentModel.ASSOC_AVATAR);
-                    if (!assocRefs.isEmpty())
+                    try
                     {
-                        avatarNodeRef = assocRefs.get(0).getTargetRef();
-                        activityFeed.setPostUserAvatarNodeRef(avatarNodeRef);
-                    }
-                    else
+                        NodeRef postPerson = this.personService.getPerson(postUserId);
+                        List<AssociationRef> assocRefs = this.nodeService.getTargetAssocs(postPerson, ContentModel.ASSOC_AVATAR);
+                        if (!assocRefs.isEmpty())
+                        {
+                            // Get the avatar for the user id, set it in the activity feed and update the cache
+                            avatarNodeRef = assocRefs.get(0).getTargetRef();
+                        }
+                    } 
+                    catch (NoSuchPersonException e) 
                     {
-                        activityFeed.setPostUserAvatarNodeRef(null);
+                        if (logger.isDebugEnabled())
+                        {
+                            logger.warn("getUserFeedEntries: person no longer exists: "+postUserId);
+                        }
                     }
                     
                     // Update the cache (setting null if there is no avatar for the user)...
-                    userIdToAvatarNodeRefCache.put(activityFeed.getPostUserId(), avatarNodeRef);
+                    userIdToAvatarNodeRefCache.put(postUserId, avatarNodeRef);
                 }
+                
+                activityFeed.setPostUserAvatarNodeRef(avatarNodeRef);
                 
                 activityFeed.setSiteNetwork(tenantService.getBaseName(activityFeed.getSiteNetwork()));
                 result.add(activityFeed);