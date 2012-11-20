992246e933fee288f98745cdc6ae3ba5c53be108
kroast <kroast@7033f55c-6c29-4ad8-afba-765cb616e738>
2012-11-20 12:15:58 +0000

    File missed from last commit (ALF-16857)
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@43727 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/wiki/toolbar.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/wiki/toolbar.get.js
@@ -19,17 +19,16 @@ function main()
    
    // Widget instantiation metadata...
    var wikiToolbar = {
-      id : "WikiToolbar", 
-      name : "Alfresco.WikiToolbar",
-      options : {
-         siteId : (page.url.templateArgs.site != null) ? page.url.templateArgs.site : "",
-         title : (page.url.templateArgs.title != null) ? page.url.templateArgs.title : "",
-         showBackLink : (args.showBackLink == "true"),
-         exists : model.exists
+      id: "WikiToolbar", 
+      name: "Alfresco.WikiToolbar",
+      options: {
+         siteId: (page.url.templateArgs.site != null) ? page.url.templateArgs.site : "",
+         title: title ? title : "",
+         showBackLink: (args.showBackLink == "true"),
+         exists: model.exists
       }
    };
    model.widgets = [wikiToolbar];
 }
 
 main();
-
