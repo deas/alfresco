e0999019d9d74a8740f9c45010e1ccb8e61991ac
dwebster <dwebster@7033f55c-6c29-4ad8-afba-765cb616e738>
2012-11-27 14:39:14 +0000

    Fixes: ALF-16840 - TinyMCE issue that crept in during widget instantiation refactoring.
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@44044 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/blog/postedit.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/blog/postedit.get.js
@@ -22,7 +22,6 @@ function main()
             theme_advanced_toolbar_align: "left",
             theme_advanced_statusbar_location: "bottom",
             theme_advanced_resizing: true,
-            theme_advanced_buttons2: null,
             theme_advanced_buttons3: null,
             theme_advanced_path: false,
             language: locale
