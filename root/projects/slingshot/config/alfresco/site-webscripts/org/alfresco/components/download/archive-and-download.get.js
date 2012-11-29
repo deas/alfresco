ee6b676f5fb811fea76a38a91c10a5b65df52b16
kroast <kroast@7033f55c-6c29-4ad8-afba-765cb616e738>
2012-11-29 15:02:05 +0000

    Refactored archive-and-download component to widget style component framework. Removed pointless copy/paste logic that appears to have come from the upload component (!)
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@44161 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/download/archive-and-download.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/download/archive-and-download.get.js
@@ -1,31 +1,11 @@
-/**
- * Custom content types
- */
-function getContentTypes()
+function main()
 {
-   // TODO: Data webscript call to return list of available types
-   var contentTypes = [
-   {
-      id: "cm:content",
-      value: "cm_content"
-   }];
-
-   return contentTypes;
+   // Widget instantiation metadata...
+   var archiveAndDownload = {
+      id: "ArchiveAndDownload", 
+      name: "Alfresco.ArchiveAndDownload"
+   };
+   model.widgets = [archiveAndDownload];
 }
 
-model.contentTypes = getContentTypes();
-
-// Get the file-upload settings...
-var _inMemoryLimit, _maximumFileSizeLimit;
-var docLibConfig = config.scoped["DocumentLibrary"];
-if (docLibConfig != null)
-{
-   var fileUpload = docLibConfig["file-upload"];
-   if (fileUpload != null)
-   {
-      _inMemoryLimit = fileUpload["in-memory-limit"];
-      _maximumFileSizeLimit = fileUpload["maximum-file-size-limit"];
-   }
-}
-model.inMemoryLimit = (_inMemoryLimit != null) ? _inMemoryLimit : "262144000";
-model.fileUploadSizeLimit = (_maximumFileSizeLimit != null) ? _maximumFileSizeLimit : "0";
+main();
