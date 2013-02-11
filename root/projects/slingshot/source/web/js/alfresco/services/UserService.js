b1c056c0cab791c8c9a8e96901cd28fd37c34b14
ddraper <ddraper@7033f55c-6c29-4ad8-afba-765cb616e738>
2013-02-11 09:52:54 +0000

    ALF-17914: Fixed CSRF issue on user status post
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@46469 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/source/web/js/alfresco/services/UserService.js
+++ root/projects/slingshot/source/web/js/alfresco/services/UserService.js
@@ -25,36 +25,50 @@ define(["dojo/_base/declare",
        * @param {object} data The payload containing the user status to post.
        */
       updateUserStatus: function alf_services_UserService__updateUserStatus(data) {
-         var _this = this;
-         xhr(Alfresco.constants.URL_SERVICECONTEXT + "components/profile/userstatus", {
-            handleAs: "text",
-            method: "POST",
-            data: JSON.stringify(data),
-            headers: { 'Content-Type': 'application/json' }
-        }).then(function(response){
-           // HANDLE SUCCESS...
-           // NOTE: The current update status API does NOT include the updated status message in the
-           //       response. Ideally it would be nice to change this such that it does to ensure
-           //       that the users status is correctly reflected. However, we will include the user
-           //       status property here in the publication payload and set it to null to indicate
-           //       that it is unknown. This is done because the UserStatus widget (at the time of 
-           //       writing the only subscriber to this publication is coded to handle status updates
-           //       that DO include a status message.
-           _this.alfLog("log", "User Status Update Success", response);
-           var response = JSON.parse(_this.cleanupJSONResponse(response));
-           _this.alfPublish("ALF_USER_STATUS_UPDATED", {
-              userStatus: null,
-              userStatusTime: response.userStatusTime.iso8601
-           });
-        }, function(response){
-           // HANDLE FAILURE...
-           _this.alfLog("log", "User Status Update Failure", response);
-           var response = JSON.parse(_this.cleanupJSONResponse(response));
-           _this.alfPublish("ALF_USER_STATUS_UPDATE_FAILURE", response);
-        }, function(evt){
-           // HANDLE PROGRESS...
-           _this.alfLog("log", "User Status Update Progress", evt);
-        });
+         var _this = this,
+             url = Alfresco.constants.URL_SERVICECONTEXT + "components/profile/userstatus";
+         this.serviceXhr({url : url,
+                          data: data,
+                          method: "POST",
+                          successCallback: this.userStatusUpdateSuccess,
+                          failureCallback: this.userStatusUpdateFailure,
+                          callbackScope: this});
+      },
+      
+      /**
+       * This handles successfully completed requests to update the user status.
+       * 
+       * @method userStatusUpdateSuccess
+       * @param {object} response The response from the request
+       * @param {object} originalRequestConfig The configuration passed on the original request
+       */
+      userStatusUpdateSuccess: function alf_services_UserService__userStatusUpdateSuccess(response, originalRequestConfig) {
+         // NOTE: The current update status API does NOT include the updated status message in the
+         //       response. Ideally it would be nice to change this such that it does to ensure
+         //       that the users status is correctly reflected. However, we will include the user
+         //       status property here in the publication payload and set it to null to indicate
+         //       that it is unknown. This is done because the UserStatus widget (at the time of 
+         //       writing the only subscriber to this publication is coded to handle status updates
+         //       that DO include a status message.
+         this.alfLog("log", "User Status Update Success", response);
+         var response = JSON.parse(this.cleanupJSONResponse(response));
+         this.alfPublish("ALF_USER_STATUS_UPDATED", {
+            userStatus: null,
+            userStatusTime: response.userStatusTime.iso8601
+         });
+      },
+      
+      /**
+       * This handles failed attempts to update the user status.
+       * 
+       * @method userStatusUpdateFailure
+       * @param {object} response The response from the request
+       * @param {object} originalRequestConfig The configuration passed on the original request
+       */
+      userStatusUpdateFailure: function alf_services_UserService__userStatusUpdateFailure(response, originalRequestConfig) {
+         this.alfLog("log", "User Status Update Failure", response);
+         var response = JSON.parse(this.cleanupJSONResponse(response));
+         this.alfPublish("ALF_USER_STATUS_UPDATE_FAILURE", response);
       }
    });
 });
