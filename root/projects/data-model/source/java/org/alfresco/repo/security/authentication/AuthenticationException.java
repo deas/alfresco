a519959bf6847fe621bf30531391efc3a76fe900
kroast <kroast@7033f55c-6c29-4ad8-afba-765cb616e738>
2013-02-15 11:44:53 +0000

    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@46677 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/data-model/source/java/org/alfresco/repo/security/authentication/AuthenticationException.java
+++ root/projects/data-model/source/java/org/alfresco/repo/security/authentication/AuthenticationException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2010 Alfresco Software Limited.
+ * Copyright (C) 2005-2012 Alfresco Software Limited.
  *
  * This file is part of Alfresco
  *
@@ -28,19 +28,88 @@ import org.alfresco.error.AlfrescoRuntimeException;
  */
 public class AuthenticationException extends AlfrescoRuntimeException
 {
-
     /**
      * 
      */
     private static final long serialVersionUID = 3546647620128092466L;
+ 
+    // Diagnostic information to assist with problem determination
+    AuthenticationDiagnostic diagnostic;
 
     public AuthenticationException(String msg)
     {
         super(msg);
+    }
+    
+    public AuthenticationException(String msg, Object[]args)
+    {
+        super(msg, args);
     }
 
     public AuthenticationException(String msg, Throwable cause)
     {
         super(msg, cause);
     }
+    
+    public AuthenticationException(String msg, Object[] args, Throwable cause)
+    {
+        super(msg, cause);
+    }
+    
+    /**
+     * Authentication Exception
+     * @param msg human readable message
+     * @param diagnostic diagnostic information about how the authentication succeeded/failed
+     */
+    public AuthenticationException(String msg, AuthenticationDiagnostic diagnostic)
+    {
+        super(msg);
+        this.diagnostic = diagnostic;
+    }
+    
+    /**
+     * Authentication Exception
+     * @param msg human readable message
+     * @param diagnostic diagnostic information about how the authentication succeeded/failed
+     */
+    public AuthenticationException(String msg, Object[] args, AuthenticationDiagnostic diagnostic)
+    {
+        super(msg, args);
+        this.diagnostic = diagnostic;
+    }
+    
+    /**
+     * Authentication Exception
+     * @param msg key for human readable message
+     * @param diagnostic diagnostic information about how the authentication succeeded/failed
+     * @param cause stack trace of the exception
+     */
+    public AuthenticationException(String msg, AuthenticationDiagnostic diagnostic, Throwable cause)
+    {
+        super(msg, cause);
+        this.diagnostic = diagnostic;
+    }
+    
+    /**
+     * Authentication Exception
+     * @param msg key for human readable message
+     * @param diagnostic diagnostic information about how the authentication succeeded/failed
+     * @param args arguments for human readable message
+     * @param cause stack trace of the exception
+     */
+    public AuthenticationException(String msg, AuthenticationDiagnostic diagnostic, Object[] args, Throwable cause)
+    {
+        super(msg, args, cause);
+        this.diagnostic = diagnostic;
+    }
+
+    /**
+     * Get the authentication diagnostic
+     * @return the authentication diagnostic
+     */
+    public AuthenticationDiagnostic getDiagnostic()
+    {
+        return diagnostic;
+    } 
+    
 }
