80d9ebe21b1d1c035f30849a4b4a52d5c479ed86
fheremans <fheremans@7033f55c-6c29-4ad8-afba-765cb616e738>
2013-02-22 13:19:54 +0000

    ALF-10243: submitDate option check done better
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@46982 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/web-framework-commons/source/web/components/form/date-picker.js
+++ root/projects/web-framework-commons/source/web/components/form/date-picker.js
@@ -301,7 +301,7 @@
             var isoValue = "";
             
             // Check if time should be submitted as well 
-            if (this.options.submitTime) 
+            if (this.options.submitTime == true) 
             {
             	isoValue = Alfresco.util.toISO8601(selDate, {"milliseconds":true});
             }
