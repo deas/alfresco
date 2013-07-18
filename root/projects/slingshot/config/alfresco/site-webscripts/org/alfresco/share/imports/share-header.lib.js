faf193253455a39f6adb523b6b3ce2bcc1ff89b2
kroast <kroast@7033f55c-6c29-4ad8-afba-765cb616e738>
2013-07-18 10:47:32 +0000

    Fix for CLOUD-1877 - 'Dashboard' and 'Members' links are not localized
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@52802 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/share/imports/share-header.lib.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/share/imports/share-header.lib.js
@@ -412,7 +412,7 @@ function getSiteNavigationWidgets() {
          name: "alfresco/menus/AlfMenuBarItem",
          config: {
             id: "HEADER_SITE_DASHBOARD",
-            label: "Dashboard",
+            label: msg.get("page.siteDashboard.title"),
             targetUrl: "site/" + page.url.templateArgs.site + "/dashboard",
             selected: (page.titleId == "page.siteDashboard.title")
          }
@@ -435,7 +435,7 @@ function getSiteNavigationWidgets() {
          name: "alfresco/menus/AlfMenuBarItem",
          config: {
             id: "HEADER_SITE_MEMBERS",
-            label: "Members",
+            label: msg.get("page.siteMembers.title"),
             targetUrl: "site/" + page.url.templateArgs.site + "/site-members",
             selected: (page.titleId == "page.siteMembers.title")
          }
