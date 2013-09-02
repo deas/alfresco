f381caf8762db2bd95ee8ec6ba39dc6dc244b5c8
mward <mward@7033f55c-6c29-4ad8-afba-765cb616e738>
2013-09-02 08:42:13 +0000

    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@54747 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ root/projects/core/source/java/org/alfresco/util/LockHelper.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2005-2013 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.util;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * Helper to make trying for read-write locks simpler
+ * 
+ * @author Derek Hulley
+ * @since 4.1.7
+ */
+public class LockHelper
+{
+    /**
+     * Exception generated when a lock try is unsuccessful
+     * 
+     * @author Derek Hulley
+     * @since 4.1.7
+     */
+    public static class LockTryException extends RuntimeException
+    {
+        private static final long serialVersionUID = -3629889029591630609L;
+
+        public LockTryException(String msg)
+        {
+            super(msg);
+        }
+    }
+    
+    /**
+     * Try to get a lock in the given number of milliseconds or get an exception
+     * 
+     * @param lock                          the lock to try
+     * @param timeoutMs                     the number of milliseconds to try
+     * @throws LockTryException    the exception if the time is exceeded or the thread is interrupted
+     */
+    public static void tryLock(Lock lock, long timeoutMs) throws LockTryException
+    {
+        boolean gotLock = false;
+        try
+        {
+            gotLock = lock.tryLock(timeoutMs, TimeUnit.MILLISECONDS);
+        }
+        catch (InterruptedException e)
+        {
+            // Handled
+        }
+        if (!gotLock)
+        {
+            throw new LockTryException("Failed to get lock " + lock + " in " + timeoutMs + "ms.");
+        }
+    }
+}
