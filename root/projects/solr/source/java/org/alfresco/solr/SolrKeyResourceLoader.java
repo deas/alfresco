3bc210a27f105482fcb8f64bb49ff326918d734e
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-02-20 14:18:27 +0000

    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@62952 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/solr/source/java/org/alfresco/solr/SolrKeyResourceLoader.java
+++ root/projects/solr/source/java/org/alfresco/solr/SolrKeyResourceLoader.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.alfresco.solr.client;
+package org.alfresco.solr;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
