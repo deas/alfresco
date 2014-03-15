ee49f0248f9e283937785d52ceef6b5edf4c1358
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-03-15 07:27:58 +0000

    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@64609 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ root/projects/qa-share/src/test/java/org/alfresco/share/admin/SiteAdminGroupTests.java
@@ -0,0 +1,348 @@
+/*
+ * Copyright (C) 2005-2014 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.share.admin;
+
+/**
+*
+* @author Charu
+* 
+*/
+import java.util.List;
+
+import org.alfresco.po.share.DashBoardPage;
+import org.alfresco.po.share.GroupsPage;
+import org.alfresco.po.share.RemoveUserFromGroupPage;
+import org.alfresco.po.share.RemoveUserFromGroupPage.Action;
+import org.alfresco.po.share.site.document.UserProfile;
+import org.alfresco.share.util.AbstractTests;
+import org.alfresco.share.util.ShareUser;
+import org.alfresco.webdrone.testng.listener.FailedTestListener;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+
+@Listeners(FailedTestListener.class)
+public class SiteAdminGroupTests extends AbstractTests
+{
+    private static final Logger logger = Logger.getLogger(SiteAdminGroupTests.class);
+    public String siteAdmin = "SITE_ADMINISTRATORS";        
+    public DashBoardPage dashBoard;
+    public String title = "Remove User from Group";
+
+    @Override
+    @BeforeClass(alwaysRun = true)
+    public void setup() throws Exception
+    {
+
+        super.setup();
+        testName = this.getClass().getSimpleName();
+
+    }
+
+    /**
+     * User logs in before test is executed
+     * 
+     * @throws Exception
+     */
+    @BeforeMethod
+    public void prepare() throws Exception
+    {
+        // login as created user
+        try
+        {
+            ShareUser.login(drone, ADMIN_USERNAME, ADMIN_PASSWORD);
+            logger.info("Create user logged in - drone.");
+        }
+        catch (Throwable e)
+        {
+            reportError(drone, testName, e);
+        }
+    }
+
+    /**
+     * User logs out before test is executed
+     * 
+     * @throws Exception
+     */
+    @AfterMethod
+    public void quit() throws Exception
+    {
+        // logout as created user
+        try
+        {
+            ShareUser.logout(drone);
+            logger.info("RepositoryDocumentCreate user logged out - drone.");
+        }
+        catch (Throwable e)
+        {
+            reportError(drone, testName, e);
+        }
+    }
+
+    /**
+     * Test:
+     * <ul>
+     * <li>Get the list of groups displayed in Groups page</li>
+     * <li>Verify Site_Administrator group is present in the group name list</li>
+     * </ul>
+     */    
+    
+    @Test (groups = "Enterprise-only")
+    public void ACE_564_02() throws Exception
+    {
+        // Open User dash board page
+        DashBoardPage dashBoardPage = drone.getCurrentPage().render();
+        
+        //Navigate to Groups page 
+        GroupsPage page = dashBoardPage.getNav().getGroupsPage();
+        
+        //Navigate to Add and Edit groups in Groups page
+        page = page.navigateToAddAndEditGroups().render();
+        
+        //Verify Site_admin group name is present in the list of Groups
+        Assert.assertTrue(page.getGroupList().contains(siteAdmin), "Site Admin Group is present!!");        
+    
+    }
+    
+    /**
+     * Test:
+     * <ul>
+     * <li>Select the Site_Admin group from the list of Groups in Groups page</li>
+     * <li>Verify Site_Admin is present in the Group Members list</li>
+     * </ul>
+     */    
+    @Test(groups = "Enterprise-only")
+    public void ACE_564_04() throws Exception
+    {
+        String admin ="Administrator";
+        String lname ="(admin)";
+        
+        //Open User dash board page
+        DashBoardPage dashBoard = drone.getCurrentPage().render();
+        
+        //Navigate to Groups page
+        GroupsPage page = dashBoard.getNav().getGroupsPage().render();
+        
+        //Navigate to Add and Edit groups in Groups page
+        page = page.navigateToAddAndEditGroups().render();
+        
+        //Verify Site_admin group name is present in the list of Groups
+        Assert.assertTrue(page.getGroupList().contains(siteAdmin), "Site Admin Group is present!!");
+        
+        //Select site_Admin group from list of groups           
+        GroupsPage groupspage = page.selectGroup(siteAdmin).render();     
+        
+        //Get the user profile for list of users in Groups page
+        List<UserProfile> userProfiles = groupspage.getMembersList();
+        
+        for (UserProfile userProfile : userProfiles)
+        {
+            if(admin.equals(userProfile.getfName()))
+            {                
+                // Verify user is present in Groups page
+                Assert.assertTrue(userProfile.getUsername().contains(lname), "User is present!!");
+               
+            } 
+        }
+      
+    }    
+    
+    /**
+     * Test:
+     * <ul>
+     * <li>Create new user and add to site admin group</li>
+     * <li>Select the Site_Admin group from the list of Groups in Groups page</li>
+     * <li>Verify new user created and added to site admin group is present in the Members list</li>
+     * <li></li>
+     * </ul>
+     */    
+    @Test(groups = "Enterprise-only")
+    public void ACE_564_06() throws Exception
+    {
+        String testname = "A" + getTestName() + System.currentTimeMillis();
+        
+        //Create User and add to Site Admin group
+        ShareUser.createEnterpriseUserWithGroup(drone, ADMIN_USERNAME, testname, testname, testname, DEFAULT_PASSWORD, siteAdmin);
+
+        //Login as Admin
+        ShareUser.login(drone, ADMIN_USERNAME, ADMIN_PASSWORD);
+        drone.refresh();
+        
+        //Navigate to Dash Board Page
+        DashBoardPage dashBoard = drone.getCurrentPage().render();
+        
+        //Navigate to Groups Page
+        GroupsPage page = dashBoard.getNav().getGroupsPage();
+        
+        //Navigate to Add and edit Groups page
+        GroupsPage groupsPage = page.navigateToAddAndEditGroups().render();
+        
+        //Verify Site_admin is present in groups list
+        Assert.assertTrue(groupsPage.getGroupList().contains(siteAdmin), "Site Admin Group is present!!");
+        
+        //Select Site_admin group from the list of groups
+        GroupsPage groupspage = groupsPage.selectGroup(siteAdmin).render();        
+                
+        //Get the user profile for list of users
+        List<UserProfile> userProfiles = groupspage.getMembersList();
+        
+        for (UserProfile userProfile : userProfiles)
+        {
+            if(testname.equals(userProfile.getfName()))
+            {
+                //Verify created user is present in the members list in groups page
+                Assert.assertTrue(userProfile.getUsername().contains(testname), "User is present!!");
+            }
+        }
+
+    }
+    
+    /**
+     * Test:
+     * <ul>
+     * <li>Create new user and add to site_admin group</li>
+     * <li>Select the Site_Admin group from the list of Groups in Groups page</li>
+     * <li>Remove created and added user from Site_admin group</li>
+     * <li>Verify Confirm remove user pop up window is displayed</li>
+     * <li>Confirm 'No'to remove user from pop up window </li>
+     * <li>Verify Created user is present in the members list in Groups page</li>
+     * <li></li>
+     * </ul>
+     */    
+    
+    @Test(groups = "Enterprise-only")
+    public void ACE_564_08() throws Exception
+    {
+        String testname = "A" + getTestName() + System.currentTimeMillis();        
+        
+        //Create User and add to Site Admin group
+        ShareUser.createEnterpriseUserWithGroup(drone, ADMIN_USERNAME, testname, testname, testname, DEFAULT_PASSWORD, siteAdmin);
+        
+        //Login as Admin
+        ShareUser.login(drone, ADMIN_USERNAME, ADMIN_PASSWORD);
+        drone.refresh();
+        
+        //Navigate to Dash Board Page
+        DashBoardPage dashBoardPage = drone.getCurrentPage().render(); 
+        
+        //Navigate to Groups Page
+        GroupsPage page = dashBoardPage.getNav().getGroupsPage();
+        
+        //Navigate to Add and edit Groups page
+        GroupsPage groupsPage = page.navigateToAddAndEditGroups().render();
+        
+        //Verify Site_admin is present in groups list
+        Assert.assertTrue(groupsPage.getGroupList().contains(siteAdmin), "Site Admin Group is present!!");
+        
+        //Select Site_admin group from the list of groups
+        GroupsPage groupspage = groupsPage.selectGroup(siteAdmin).render();
+        
+        //Remove user from members list in Groups page
+        RemoveUserFromGroupPage removeUserFromGroupPagegroupspage = groupspage.removeUser(testname);
+        
+        //Verify Confirm Remove pop up window is displayed
+        Assert.assertTrue(removeUserFromGroupPagegroupspage.getTitle().equalsIgnoreCase(title), "Title is present");
+        
+        //Confirm Remove user from Group page
+        removeUserFromGroupPagegroupspage.selectAction(Action.No).render();
+        
+        List<UserProfile> userProfiles = groupspage.getMembersList();
+        
+        for (UserProfile userProfile : userProfiles)
+        {
+            if(testname.equals(userProfile.getfName()))
+            {
+                //Verify user is present  in the members list  
+                Assert.assertTrue(userProfile.getUsername().contains(testname));
+                break;
+            }
+        }    
+                     
+    }       
+
+    /**
+     * Test:
+     * <ul>
+     * <li>Create new user and add to site_admin group</li>
+     * <li>Select the Site_Admin group from the list of Groups in Groups page</li>
+     * <li>Remove created and added user from Site_admin group</li>
+     * <li>Verify Confirm remove user pop up window is displayed</li>
+     * <li>Confirm 'Yes'to remove user from pop up window </li>
+     * <li>Verify created user is removed from members list in Groups page</li>
+     * <li></li>
+     * </ul>
+     */    
+    @Test(groups = "Enterprise-only")
+    public void ACE_564_10() throws Exception
+    {
+        String testname = "A" + getTestName() + System.currentTimeMillis();
+        
+        //Create User and add to Site Admin group
+        ShareUser.createEnterpriseUserWithGroup(drone, ADMIN_USERNAME, testname, testname, testname, DEFAULT_PASSWORD, siteAdmin);
+        
+        //Login as Admin
+        ShareUser.login(drone, ADMIN_USERNAME, ADMIN_PASSWORD);
+        drone.refresh();
+        
+        //Navigate to Dash Board Page
+        DashBoardPage dashBoardPage = drone.getCurrentPage().render(); 
+        
+        //Navigate to Groups Page
+        GroupsPage page = dashBoardPage.getNav().getGroupsPage();
+        
+        //Navigate to Add and edit Groups page
+        GroupsPage groupsPage = page.navigateToAddAndEditGroups().render();
+        
+        //Verify Site_admin is present in groups list
+        Assert.assertTrue(groupsPage.getGroupList().contains(siteAdmin), "Site Admin Group is present!!");
+        
+        //Select Site_admin group from the list of groups
+        GroupsPage groupspage = groupsPage.selectGroup(siteAdmin).render();
+        
+        //Remove user from members list in Groups page
+        RemoveUserFromGroupPage removeUserFromGroupPagegroupspage = groupspage.removeUser(testname);
+        
+        //Verify Confirm Remove pop up window is displayed
+        Assert.assertTrue(removeUserFromGroupPagegroupspage.getTitle().equalsIgnoreCase(title), "Title is present");
+        
+        //Confirm Remove user from Group page
+        removeUserFromGroupPagegroupspage.selectAction(Action.Yes).render();
+        
+        List<UserProfile> userProfiles = groupspage.getMembersList();
+        
+        for (UserProfile userProfile : userProfiles)
+        {
+            if(testname.equals(userProfile.getfName()))
+            {
+                //Verify user is not displayed in the members list  
+                Assert.assertFalse(userProfile.getUsername().contains(testname));
+                break;
+            }
+        }    
+                     
+    }
+       
+}
+
+    
