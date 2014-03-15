8f324cb8699a6b2a05287974e157692b5620d0ac
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-03-15 07:24:44 +0000

    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@64606 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/share-po/src/main/java/org/alfresco/po/share/GroupsPage.java
+++ root/projects/share-po/src/main/java/org/alfresco/po/share/GroupsPage.java
@@ -22,65 +22,85 @@ import static org.alfresco.webdrone.RenderElement.getVisibleRenderElement;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
+import org.alfresco.po.share.site.document.CopyOrMoveContentPage;
+import org.alfresco.po.share.site.document.UserProfile;
 import org.alfresco.webdrone.ElementState;
 import org.alfresco.webdrone.RenderElement;
 import org.alfresco.webdrone.RenderTime;
 import org.alfresco.webdrone.WebDrone;
+import org.alfresco.webdrone.WebDroneUtil;
+import org.alfresco.webdrone.exception.PageException;
+import org.alfresco.webdrone.exception.PageOperationException;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 
 /**
- * @author nshah
- * To create Groups this page object gets used.
+ * @author Charu To get the list of groups and group members this page is used 
  */
 public class GroupsPage extends SharePage
 {
     private static final String SHOW_ALL_LABEL = "label[for$='_default-show-all']";
     private static final String SHOW_ALL_CHK_BOX = "input[id$='_default-show-all']";
 
-    private static final String BUTTON_BROWSE= "button[id$='default-browse-button-button']";
-    private static final String BUTTON_SEARCH= "button[id$='default-search-button-button']";
+    private static final String BUTTON_BROWSE = "button[id$='default-browse-button-button']";
+    private static final String BUTTON_SEARCH = "button[id$='default-search-button-button']";
     private static final String BUTTON_ADD = ".groups-newgroup-button";
+    private static final String BUTTON_ADD_USER = ".groups-adduser-button";
+    private static final String BUTTON_ADD_GROUP = ".groups-addgroup-button";
     private static final String GROUP_NAMES = "a[class$='groups-item-group']";
+    private static Log logger = LogFactory.getLog(CopyOrMoveContentPage.class);
+    private static final String USER_NAMES = "a[class*='groups-item-user']>span[class$='-item-label']";    
+    public static String ADD_GROUP = "td[class*='yui-dt-col-actions'] button";
+    public static String ADD_USER = "td[class*='yui-dt-col-actions'] button";
 
     public GroupsPage(WebDrone drone)
     {
-        super(drone);       
+        super(drone);
     }
 
     @SuppressWarnings("unchecked")
     @Override
-    public GroupsPage  render(RenderTime timer)
+    public GroupsPage render(RenderTime timer)
     {
         RenderElement actionMessage = getActionMessageElement(ElementState.INVISIBLE);
-        elementRender(timer, getVisibleRenderElement(By.cssSelector(BUTTON_BROWSE)), getVisibleRenderElement(By.cssSelector(BUTTON_SEARCH)),
-                        getVisibleRenderElement(By.cssSelector(SHOW_ALL_LABEL)), getVisibleRenderElement(By.cssSelector(SHOW_ALL_CHK_BOX)), actionMessage);
-       return this;
+        elementRender(timer, getVisibleRenderElement(By.cssSelector(BUTTON_BROWSE)),
+        getVisibleRenderElement(By.cssSelector(BUTTON_SEARCH)),
+        getVisibleRenderElement(By.cssSelector(SHOW_ALL_LABEL)),
+        getVisibleRenderElement(By.cssSelector(SHOW_ALL_CHK_BOX)), actionMessage);
+        return this;
     }
+
     @SuppressWarnings("unchecked")
     @Override
     public GroupsPage render(final long time)
     {
         return render(new RenderTime(time));
     }
+
     @SuppressWarnings("unchecked")
     @Override
     public GroupsPage render()
     {
         return render(new RenderTime(maxPageLoadingTime));
     }
-    
+
     /**
      * @return
      */
     public GroupsPage navigateToAddAndEditGroups()
     {
         drone.findAndWait(By.cssSelector(BUTTON_BROWSE)).click();
-        
+
         return this;
     }
-    
+
     /**
      * @return
      */
@@ -89,9 +109,10 @@ public class GroupsPage extends SharePage
         drone.findAndWait(By.cssSelector(BUTTON_ADD)).click();
         return new NewGroupPage(drone);
     }
-    
+
     /**
      * Get list of available groups.
+     * 
      * @return
      */
     public List<String> getGroupList()
@@ -104,5 +125,219 @@ public class GroupsPage extends SharePage
         }
         return nameOfGroups;
     }
+    
+    /**
+     * Select Group name  from available group list in the Groups page
+     * 
+     * @param groupName -To select this group name from the list of groups 
+     * 
+     * @return {@link GroupsPage}
+     */
+    public GroupsPage selectGroup(String groupName)
+    {
+        WebDroneUtil.checkMandotaryParam("Group Name", groupName);
+        try
+        {
+            for (WebElement name : drone.findAndWaitForElements(By.cssSelector(GROUP_NAMES)))
+            {
+                if (groupName.equalsIgnoreCase(name.getText()))
+                {
+                    name.click();
+                    return this;
+                }
+            }
+        }
+        catch (TimeoutException e)
+        {
+            logger.error("Unable to get the group from list of Groups : " + e.getMessage());
+        }
+
+        throw new PageOperationException("Unable to select group : " + groupName);
+    }
+
+    /**
+     * Assert method to verify any group name is present in the list of groups in groups page
+     * 
+     * @param groupName -To verify this group name is present in the the list of groups
+     * 
+     * @return Boolean
+     */
+    public boolean isGroupPresent(String GroupName)
+    {
+        WebDroneUtil.checkMandotaryParam("Group Name", GroupName);
+        try
+        {
+            List<WebElement> groupList = drone.findAll(By.cssSelector(GROUP_NAMES));
+
+            for (WebElement groupName : groupList)
+            {
+                if (groupName.getText().contains(GroupName))
+                {
+                    return true;
+                }
+            }
+        } 
+        catch (NoSuchElementException e)
+        {
+            if (logger.isTraceEnabled())
+            {
+                logger.trace("Unable to find group in list");
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get list of group members for any group in groups page
+     * 
+     * @return List of Users
+     */
+    public List<UserProfile> getMembersList()
+    {
+        try
+        {
+            List<UserProfile> listOfUsers = new ArrayList<UserProfile>();
+            List<WebElement> groupMembers = drone.findAndWaitForElements(By.cssSelector(USER_NAMES));            
+            for (WebElement webElement : groupMembers)
+            {
+                UserProfile profile = new UserProfile();
+                String text = webElement.getText();
+                StringTokenizer userInfo = new StringTokenizer(text);
+                
+                int userDispalySize = 0;
+                int userInfoSize = userInfo.countTokens();
+                
+                while (userInfo.hasMoreElements())
+                {
+                   
+                        if (userDispalySize == 0)
+                        {
+                            profile.setfName((String) userInfo.nextElement());
+                        } else if (userDispalySize == 1 && userInfoSize>2 )
+                        {
+                            profile.setlName((String) userInfo.nextElement());
+                        } else if (userDispalySize == 2 || (userDispalySize==1 && userInfoSize == 2) )
+                        {
+                            profile.setUsername((String) userInfo.nextElement());
+                        }
+                    userDispalySize++;
+                }
+                listOfUsers.add(profile);
+            }
+            return listOfUsers;
+
+        } catch (TimeoutException e)
+        {
+            logger.error("Unable to get the list of members : " + e.getMessage());
+        }
+
+        throw new PageOperationException("Unable to get the list of members  : ");
+    }
+    
+    /**
+     * click on Add user button in GroupsPage
+     * 
+     * @return AddUserPage
+     */
+    public AddUserPage clickAddUser()
+    {
+        try
+        {
+            WebElement addUserButton = drone.findAndWait(By.cssSelector(BUTTON_ADD_USER));
+
+            if (addUserButton.isDisplayed() && addUserButton.isEnabled())
+            {
+                addUserButton.click();
+                return new AddUserPage(drone);
+            }
+
+        } catch (TimeoutException nse)
+        {
+            logger.error("Unable to find Add User button");
+        }
+        throw new PageOperationException("Not found element is : " + BUTTON_ADD_USER);
+    }
+
+    /**
+     * Click on Add Group button in GroupsPage
+     * 
+     * @return AddGroupPage
+     */
+    public AddGroupPage clickAddGroup()
+    {
+        try
+        {
+            WebElement addGroupButton = drone.findAndWait(By.cssSelector(BUTTON_ADD_GROUP));
+
+            if (addGroupButton.isDisplayed())
+            {
+                addGroupButton.click();
+                return new AddGroupPage(drone);
+            }
+
+        } catch (TimeoutException nse)
+        {
+            logger.error("Unable to find Add Group button");
+        }
+        throw new PageOperationException("Not found element is : " + BUTTON_ADD_GROUP);
+    }
+
+    /**
+     * Click on Remove icon on members list in group page
+     * 
+     *@param userName -To remove the userName from the list of Users in the groups page
+     * 
+     * @return RemoveUserFromGroupPage
+     */
+    public RemoveUserFromGroupPage removeUser(String userName)
+    {
+        if (StringUtils.isEmpty(userName))
+        {
+            throw new IllegalArgumentException("User Name is required.");
+        }
+
+        try
+        {
+            WebElement element = drone.findAndWait(By.xpath(String.format(".//span[contains(text(),'%s')]/..", userName)));
+            drone.mouseOverOnElement(element);
+            element = element.findElement(By.cssSelector("span.yui-columnbrowser-item-buttons>span.users-remove-button"));
+            drone.mouseOver(element);
+            element.click();
+            return new RemoveUserFromGroupPage(getDrone());
+        } catch (TimeoutException e)
+        {
+            if (logger.isTraceEnabled())
+            {
+                logger.trace("User: \"" + userName + "\" can not be found in members list.");
+            }
+        }
+        throw new PageException("User: \"" + userName + "\" can not be found in members list.");
+    }
+
+    /**
+     * Verify list of users are displayed in Group page
+     * 
+     * @return Boolean
+     */
+    public boolean hasMembers()
+    {
+
+        try
+        {
+            WebElement element = drone.find(By.cssSelector(USER_NAMES));
+            if (element.isDisplayed())
+            {
+                return true;
+            }
+
+        } catch (NoSuchElementException e)
+        {
+            if (logger.isTraceEnabled())
+            {
+                logger.trace("Unable to find member in  list");
+            }
+        }
+        return false;
+    }    
 
 }
--- root/projects/share-po/src/test/java/org/alfresco/po/share/GroupPageTest.java
+++ root/projects/share-po/src/test/java/org/alfresco/po/share/GroupPageTest.java
@@ -18,7 +18,11 @@
  */
 package org.alfresco.po.share;
 
+import java.util.List;
+
 import org.alfresco.po.share.NewGroupPage.ActionButton;
+import org.alfresco.po.share.RemoveUserFromGroupPage.Action;
+import org.alfresco.po.share.site.document.UserProfile;
 import org.alfresco.po.share.util.FailedTestListener;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -34,12 +38,13 @@ import org.testng.annotations.Test;
 public class GroupPageTest extends AbstractTest
 {
     private DashBoardPage dashBoard;
-    private String groupName = "testGrp"+System.currentTimeMillis();
+    private String groupName = "testGrp"+System.currentTimeMillis();    
+    private String siteAdmin = "SITE_ADMINISTRATORS";
 
     @BeforeClass(groups = "Enterprise-only")
     public void setup() throws Exception
     {
-        dashBoard = loginAs(username, password);
+        dashBoard = loginAs("admin", "admin");
     }
 
     @Test(groups = "Enterprise-only")
@@ -49,8 +54,104 @@ public class GroupPageTest extends AbstractTest
         page = page.navigateToAddAndEditGroups().render();
         NewGroupPage newGroupPage = page.navigateToNewGroupPage().render();
         page = newGroupPage.createGroup(groupName, groupName, ActionButton.CREATE_GROUP).render();
-        Assert.assertTrue(page.getGroupList().contains(groupName), String.format("Group: %s can not be found", groupName));
+        Assert.assertTrue(page.getGroupList().contains(groupName), "Group is created!!");
        
     }
     
+    @Test(groups = "Enterprise-only")
+    public void testSelectGroup() throws Exception
+    {         
+        GroupsPage page = dashBoard.getNav().getGroupsPage();
+        page = page.navigateToAddAndEditGroups().render();
+        Assert.assertTrue(page.getGroupList().contains(siteAdmin), "Site Admin Group is present!!");
+        GroupsPage groupsPage = page.selectGroup(siteAdmin).render();
+        Assert.assertTrue(groupsPage.hasMembers(), "Group members are  present");
+        
+    }
+    
+    @Test(groups = "Enterprise-only", dependsOnMethods = "testSelectGroup")
+    public void testisGroupPresent() throws Exception
+    {         
+        GroupsPage page = dashBoard.getNav().getGroupsPage();
+        page = page.navigateToAddAndEditGroups().render();
+        Assert.assertTrue(page.getGroupList().contains(siteAdmin), "Site Admin Group is present!!");
+        
+    }
+    
+    @Test(groups = "Enterprise-only", dependsOnMethods = "testisGroupPresent")
+    public void testgetMembersList() throws Exception
+    {
+        // String Admin = "Administrator";
+
+        UserSearchPage userPage = dashBoard.getNav().getUsersPage().render();
+        NewUserPage newPage = userPage.selectNewUser().render();
+        String userinfo = "user" + System.currentTimeMillis() + "@test.com";
+        newPage.createEnterpriseUserWithGroup(userinfo, userinfo, userinfo, userinfo, userinfo, siteAdmin);
+        GroupsPage page = dashBoard.getNav().getGroupsPage();
+        page = page.navigateToAddAndEditGroups().render();
+        GroupsPage groupspage = page.selectGroup(siteAdmin).render();
+        Assert.assertTrue(groupspage.hasMembers(), "Group members are present");
+        List<UserProfile> userProfiles = groupspage.getMembersList();
+
+        for (UserProfile userProfile : userProfiles)
+        {
+            if (userinfo.equals(userProfile.getfName()))
+            {
+                Assert.assertTrue(userProfile.getlName().contains(userinfo));
+                Assert.assertTrue(userProfile.getUsername().contains(userinfo));
+                break;
+            }
+
+        }
+
+    }
+    
+    
+    @Test(groups = "Enterprise-only", dependsOnMethods = "testgetMembersList")
+    public void testclickAddUser() throws Exception
+    {         
+        GroupsPage page = dashBoard.getNav().getGroupsPage();
+        page = page.navigateToAddAndEditGroups().render();
+        page.selectGroup(siteAdmin).render();
+        AddUserPage addUserPage = page.clickAddUser().render();
+        Assert.assertTrue(addUserPage.getTitle().contains("Add User"),"Add User page is displayed");
+        
+    }
+    
+    @Test(groups = "Enterprise-only", dependsOnMethods = "testclickAddUser")
+    public void testclickAdGroup() throws Exception
+    {         
+        GroupsPage page = dashBoard.getNav().getGroupsPage();
+        page = page.navigateToAddAndEditGroups().render();
+        page.selectGroup(siteAdmin).render();
+        AddGroupPage addGroupPage = page.clickAddGroup().render();
+        Assert.assertTrue(addGroupPage.getTitle().contains("Add Group"),"Add User page is displayed");
+        
+    }
+    
+    @Test(groups = "Enterprise-only", dependsOnMethods = "testclickAdGroup")
+    public void testRemoveUser() throws Exception
+    {
+        UserSearchPage userPage = dashBoard.getNav().getUsersPage().render();
+        NewUserPage newPage = userPage.selectNewUser().render();
+        String userinfo = "user" + System.currentTimeMillis() + "@test.com";
+        newPage.createEnterpriseUserWithGroup(userinfo, userinfo, userinfo, userinfo, userinfo, siteAdmin);
+        GroupsPage page = dashBoard.getNav().getGroupsPage();
+        page = page.navigateToAddAndEditGroups().render();
+        GroupsPage groupspage = page.selectGroup(siteAdmin).render();
+        RemoveUserFromGroupPage removeUserFromGroupPage = groupspage.removeUser(userinfo).render();
+        removeUserFromGroupPage.selectAction(Action.Yes).render();
+        List<UserProfile> userProfiles = groupspage.getMembersList();
+
+        for (UserProfile userProfile : userProfiles)
+        {
+            if (userinfo.equals(userProfile.getfName()))
+            {
+                Assert.assertFalse(userProfile.getUsername().contains(userinfo));
+                break;
+            }
+
+        }
+
+    }
 }
