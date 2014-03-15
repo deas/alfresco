b9ce493e0a30befc5c6e6b77ba11b7e66e3dbc89
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-03-15 07:19:21 +0000

    Merged HEAD-BUG-FIX (4.3/Cloud) to HEAD (4.3/Cloud)    64455: Merged WAT1 (4.3/Cloud) to HEAD-BUG-FIX (4.3/Cloud)       63418: Fixes race condition in site listing - pubsub scope was hard coded but event was triggered globally.
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@64601 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/source/web/js/alfresco/documentlibrary/AlfSitesList.js
+++ root/projects/slingshot/source/web/js/alfresco/documentlibrary/AlfSitesList.js
@@ -61,7 +61,7 @@ define(["dojo/_base/declare",
 
          // Set a response topic that is scoped to this widget...
          var documentPayload = {
-            responseTopic: this.pubSubScope + "ALF_RETRIEVE_DOCUMENTS_REQUEST"
+            responseTopic: "ALF_RETRIEVE_DOCUMENTS_REQUEST"
          };
 
          if (this.site != null)
@@ -75,7 +75,7 @@ define(["dojo/_base/declare",
             documentPayload.pageSize = this.currentPageSize;
          }
 
-         this.alfPublish(this.dataRequestTopic, documentPayload, true);
+         this.alfPublish(this.dataRequestTopic, documentPayload);
       },
 
       /**
