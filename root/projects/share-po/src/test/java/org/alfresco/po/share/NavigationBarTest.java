6eb0b133e0421986018a2b87db20bbf12a0ec278
rsmith2 <rsmith2@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-03-20 10:41:45 +0000

    ACE-1111: Localization Updates required for Manage Sites   - Update to test with new localisation value
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@64925 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/share-po/src/test/java/org/alfresco/po/share/NavigationBarTest.java
+++ root/projects/share-po/src/test/java/org/alfresco/po/share/NavigationBarTest.java
@@ -1,18 +1,14 @@
 /*
  * Copyright (C) 2005-2012 Alfresco Software Limited.
- *
  * This file is part of Alfresco
- *
  * Alfresco is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
  * Alfresco is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -40,80 +36,86 @@ import org.testng.annotations.Test;
  * @since 1.0
  */
 @Listeners(FailedTestListener.class)
-@Test(groups ={"alfresco-one"})
+@Test(groups = { "alfresco-one" })
 public class NavigationBarTest extends AbstractTest
 {
-    private SharePage page;    
-    
+    private SharePage page;
+
     @BeforeClass()
     public void setup() throws Exception
     {
         page = loginAs(username, password);
     }
-    
+
     /**
      * Navigate to people finder from the dashboard page
-     * and back to dash board page by selecting the 
+     * and back to dash board page by selecting the
      * navigation icons.
+     * 
      * @throws Exception if error
      */
-    @Test(groups={"alfresco-one"})
+    @Test(groups = { "alfresco-one" })
     public void navigateToPeopleFinder() throws Exception
     {
         PeopleFinderPage peoplePage = page.getNav().selectPeople().render();
-        Assert.assertEquals("People Finder", peoplePage.getPageTitle());
+        Assert.assertEquals(peoplePage.getPageTitle(), "People Finder");
     }
-    
+
     /**
      * Test navigating to site finder page.
+     * 
      * @throws Exception if error
      */
-    @Test(dependsOnMethods = "navigateToPeopleFinder",groups={"alfresco-one"})
+    @Test(dependsOnMethods = "navigateToPeopleFinder", groups = { "alfresco-one" })
     public void navigateToSearchForSites() throws Exception
     {
         page = page.getNav().selectSearchForSites().render();
-        Assert.assertEquals("Site Finder", page.getPageTitle());
+        Assert.assertEquals(page.getPageTitle(), "Site Finder");
     }
-    
+
     /**
      * Test navigating to create site page.
+     * 
      * @throws Exception if error
      */
-    @Test(dependsOnMethods = "navigateToSearchForSites",groups={"alfresco-one"})
+    @Test(dependsOnMethods = "navigateToSearchForSites", groups = { "alfresco-one" })
     public void navigateToCreateSite() throws Exception
     {
         CreateSitePage createSitePage = page.getNav().selectCreateSite().render();
         Assert.assertTrue(createSitePage.isCreateSiteDialogDisplayed());
         createSitePage.cancel();
     }
-    
+
     /**
      * Test navigating to my profile page.
+     * 
      * @throws Exception if error
      */
-    @Test(dependsOnMethods= "navigateToCreateSite" ,groups={"alfresco-one"})
+    @Test(dependsOnMethods = "navigateToCreateSite", groups = { "alfresco-one" })
     public void navigateToMyProfile() throws Exception
     {
         MyProfilePage myProfilePage = page.getNav().selectMyProfile().render();
         Assert.assertTrue(myProfilePage.titlePresent());
     }
-    
+
     /**
      * Test navigating to change password page.
+     * 
      * @throws Exception if error
      */
-    @Test(dependsOnMethods= "navigateToMyProfile",groups={"alfresco-one"})
+    @Test(dependsOnMethods = "navigateToMyProfile", groups = { "alfresco-one" })
     public void navigateChangePassword() throws Exception
     {
         ChangePasswordPage changePasswordPage = page.getNav().selectChangePassword().render();
         Assert.assertTrue(changePasswordPage.formPresent());
     }
-    
-     /**
+
+    /**
      * Test navigating to change password page.
+     * 
      * @throws Exception if error
      */
-    @Test(dependsOnMethods= "navigateChangePassword",groups={"alfresco-one"})
+    @Test(dependsOnMethods = "navigateChangePassword", groups = { "alfresco-one" })
     public void navigateDashBoard() throws Exception
     {
         DashBoardPage dash = page.getNav().selectMyDashBoard().render();
@@ -121,54 +123,56 @@ public class NavigationBarTest extends AbstractTest
         dash.getTitle().contains("Dashboard");
         Assert.assertTrue(dash.getTitle().contains("Dashboard"));
     }
-    
+
     /**
      * Test repository link, note that this is for non cloud product.
+     * 
      * @throws Exception if error
      */
-    @Test(dependsOnMethods= "navigateDashBoard",groups = "Enterprise-only")
+    @Test(dependsOnMethods = "navigateDashBoard", groups = "Enterprise-only")
     public void navigateToRepository() throws Exception
     {
         AlfrescoVersion version = drone.getProperties().getVersion();
-        if(version.isCloud())
+        if (version.isCloud())
         {
             throw new SkipException("This feature is not supported in cloud so skip it");
         }
         RepositoryPage repoPage = page.getNav().selectRepository().render();
         Assert.assertTrue(repoPage.isBrowserTitle("Repository"));
     }
-    
+
     /**
      * Test advance search link.
      * Note supported in cloud.
+     * 
      * @throws Exception if error
      */
-    @Test(dependsOnMethods= "navigateToRepository",groups= "Enterprise-only")
+    @Test(dependsOnMethods = "navigateToRepository", groups = "Enterprise-only")
     public void advanceSearch() throws Exception
     {
-    	AlfrescoVersion version = drone.getProperties().getVersion();
-        if(version.isCloud())
+        AlfrescoVersion version = drone.getProperties().getVersion();
+        if (version.isCloud())
         {
             throw new SkipException("This feature is not supported in cloud so skip it");
         }
         AdvanceSearchContentPage searchPage = page.getNav().selectAdvanceSearch().render();
-        Assert.assertEquals("Advanced Search", searchPage.getPageTitle());
+        Assert.assertEquals(searchPage.getPageTitle(), "Advanced Search");
     }
 
-    @Test(dependsOnMethods = "advanceSearch", groups = {"Enterprise-only"}, expectedExceptions = UnsupportedOperationException.class)
+    @Test(dependsOnMethods = "advanceSearch", groups = { "Enterprise-only" }, expectedExceptions = UnsupportedOperationException.class)
     public void testSelectNetworkDropdownInEnterprise() throws Exception
     {
         page.getNav().selectNetworkDropdown();
     }
-    
-    @Test(dependsOnMethods = "testSelectNetworkDropdownInEnterprise", groups = {"Enterprise-only"}, expectedExceptions = UnsupportedOperationException.class)
+
+    @Test(dependsOnMethods = "testSelectNetworkDropdownInEnterprise", groups = { "Enterprise-only" }, expectedExceptions = UnsupportedOperationException.class)
     public void testSelectNetworkInEnterprise() throws Exception
     {
         String strInvitedUser = username.substring(username.lastIndexOf("@") + 1, username.length());
-       page.getNav().selectNetwork(strInvitedUser);
+        page.getNav().selectNetwork(strInvitedUser);
     }
-    
-    @Test(dependsOnMethods = "testSelectNetworkInEnterprise", groups = "Cloud-only")    
+
+    @Test(dependsOnMethods = "testSelectNetworkInEnterprise", groups = "Cloud-only")
     public void testNetworkDropdown()
     {
         Assert.assertNotNull(page.getNav().selectNetworkDropdown());
@@ -200,28 +204,29 @@ public class NavigationBarTest extends AbstractTest
         List<String> userNetworks = page.getNav().getUserNetworks();
         Assert.assertTrue(userNetworks.size() > 0);
     }
-    
+
     /**
      * Test navigating to Account Settings Page.
+     * 
      * @throws Exception if error
      */
-    @Test(dependsOnMethods= "testgetNetworks",groups={"Cloud-only"})
+    @Test(dependsOnMethods = "testgetNetworks", groups = { "Cloud-only" })
     public void navigateAccountSettings() throws Exception
     {
-        AccountSettingsPage accountSettingsPage = page.getNav().selectAccountSettingsPage().render();        
-        Assert.assertEquals("Account Settings", accountSettingsPage.getPageTitle());       
+        AccountSettingsPage accountSettingsPage = page.getNav().selectAccountSettingsPage().render();
+        Assert.assertEquals(accountSettingsPage.getPageTitle(), "Account Settings");
     }
-    
+
     /**
      * Navigate to admin tools from the dashboard page.
      * 
      * @throws Exception if error
      */
-    @Test(groups = {"Enterprise-only"})
+    @Test(groups = { "Enterprise-only" })
     public void navigateToAdminTools() throws Exception
     {
         AdminConsolePage adminConsolePage = page.getNav().selectAdminTools().render();
-        Assert.assertEquals("Admin Console", adminConsolePage.getPageTitle());
+        Assert.assertEquals(adminConsolePage.getPageTitle(), "Admin Console");
     }
 
     /**
@@ -233,28 +238,26 @@ public class NavigationBarTest extends AbstractTest
     public void navigateToManageSites() throws Exception
     {
         ManageSitesPage manageSitesPage = page.getNav().selectManageSitesPage().render();
-        Assert.assertEquals("Manage Sites", manageSitesPage.getPageTitle());
-    } 
-    
+        Assert.assertEquals(manageSitesPage.getPageTitle(), "Sites Manager");
+    }
+
     /**
      * Navigate to manage sites from the dashboard page by Repo Admin
      * 
      * @throws Exception if error
      */
-    @Test (groups = { "Enterprise-only" })
+    @Test(groups = { "Enterprise-only" })
     public void navigateToManageSitesSiteAdmin() throws Exception
-    {   
+    {
         String siteAdmin = "SITE_ADMINISTRATORS";
         UserSearchPage userPage = page.getNav().getUsersPage().render();
         NewUserPage newPage = userPage.selectNewUser().render();
-        String userinfo = "user" + System.currentTimeMillis() + "@test.com";        
+        String userinfo = "user" + System.currentTimeMillis() + "@test.com";
         newPage.createEnterpriseUserWithGroup(userinfo, userinfo, userinfo, userinfo, userinfo, siteAdmin);
         ShareUtil.logout(drone);
         ShareUtil.loginAs(drone, shareUrl, userinfo, userinfo);
         ManageSitesPage manageSitesPage = page.getNav().selectManageSitesSiteAdmin().render();
-        Assert.assertEquals("Manage Sites", manageSitesPage.getPageTitle());
-        
+        Assert.assertEquals(manageSitesPage.getPageTitle(), "Sites Manager");
+
     }
-    
-    
- }
+}
