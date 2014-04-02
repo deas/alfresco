c2fcfaceeb22384f3e2fbbbfb0b847d004f295e2
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-04-02 21:03:06 +0000

    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@66231 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/solr/source/java/org/alfresco/solr/AlfrescoCoreAdminHandler.java
+++ root/projects/solr/source/java/org/alfresco/solr/AlfrescoCoreAdminHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2014 Alfresco Software Limited.
+ * Copyright (C) 2005-2013 Alfresco Software Limited.
  *
  * This file is part of Alfresco
  *
@@ -7433,7 +7433,6 @@ public class AlfrescoCoreAdminHandler extends CoreAdminHandler
         IndexHealthReport indexHealthReport = tracker.checkIndex(fromTx, toTx, fromAclTx, toAclTx, fromTime, toTime);
 
         NamedList<Object> ihr = new SimpleOrderedMap<Object>();
-        ihr.add("Alfresco version", tracker.getAlfrescoVersion());
         ihr.add("DB transaction count", indexHealthReport.getDbTransactionCount());
         ihr.add("DB acl transaction count", indexHealthReport.getDbAclTransactionCount());
         ihr.add("Count of duplicated transactions in the index", indexHealthReport.getDuplicatedTxInIndex().cardinality());
