456dad9bfad7f91fcfe881a4786e7358de7db022
mrogers <mrogers@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-04-30 16:28:51 +0000

    Merged HEAD-BUG-FIX (4.3/Cloud) to HEAD (4.3/Cloud)    68193: Merged PLATFORM1 (4.3/Cloud) to HEAD-BUG-FIX (4.3/Cloud)       63676: ACE-475: update JLAN references to Hazelcast 3.1.5 from 2.4.
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@68457 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/alfresco-jlan/runsrvcl.bat
+++ root/projects/alfresco-jlan/runsrvcl.bat
@@ -1,3 +1,3 @@
 @echo off
 echo "Alfresco JLAN Server starting, enter 'x' to shutdown server, 'r' to restart server ..."
-java -Xms512m -Xmx1024m -Djava.library.path=.\jni -cp .\jars/alfresco-jlan-full.jar;.\libs\cryptix-jce-provider.jar;.\service\wrapper.jar;.\hazelcast\hazelcast-2.4.jar;.\libs\mysql-connector-java-5.1.15-bin.jar -Djava.net.preferIPv4Stack=true org.alfresco.jlan.app.JLANServer jlanConfigCluster.xml
+java -Xms512m -Xmx1024m -Djava.library.path=.\jni -cp .\jars/alfresco-jlan-full.jar;.\libs\cryptix-jce-provider.jar;.\service\wrapper.jar;.\hazelcast\hazelcast-3.1.5.jar;.\libs\mysql-connector-java-5.1.15-bin.jar -Djava.net.preferIPv4Stack=true org.alfresco.jlan.app.JLANServer jlanConfigCluster.xml
