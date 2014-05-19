0934e019b2afaa4118c51a158b81f5fb3124047a
ddraper <ddraper@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-05-19 12:19:26 +0000

    Merged HEAD-BUG-FIX (4.3/Cloud) to HEAD (4.3/Cloud)    70552: Merged WAT1 (4.3/Cloud) to HEAD-BUG-FIX (4.3/Cloud)       66927: ACE-1338: Update page model to get sort properties from Share config         - Modify faceted-search.get.js to use 'sorts' from configuration         - Added sort localisation strings to the faceted-search.get.properties
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@70783 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/share/pages/faceted-search/faceted-search.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/share/pages/faceted-search/faceted-search.get.js
@@ -1,6 +1,9 @@
 <import resource="classpath:/alfresco/site-webscripts/org/alfresco/share/imports/share-header.lib.js">
 <import resource="classpath:/alfresco/site-webscripts/org/alfresco/share/imports/share-footer.lib.js">
 
+// Get Search sorting configuration from share-config
+var sortConfig = config.scoped["Search"]["sorting"];
+
 // Get the initial header services and widgets...
 var services = getHeaderServices(),
     widgets = getHeaderModel(msg.get("faceted-search.page.title"));
@@ -91,6 +94,55 @@ var facets = {
    }
 };
 
+// Function to compose the sort fields from share-config
+function getSortFieldsFromConfig()
+{
+   // Get sort fields element from the configuration
+   var configSortFields = sortConfig.getChildren();
+
+   // Initialise the sort fields array
+   var sortFields = new Array(configSortFields.length);
+
+   // Iterate over configuration sort fields
+   for(var i=0; i < configSortFields.size(); i+=1)
+   {
+      // Extract sort properties from configuration
+      var configSortField = configSortFields.get(i),
+          label = String(configSortField.attributes["labelId"]),
+          valueTokens = String(configSortField.value).split("|"),
+          value = valueTokens[0],
+          direction = "descending",
+          checked = (i==0 ? true : false);
+
+      // The value may contain 2 pieces of data - the optional 2nd is for sort direction
+      if(valueTokens instanceof Array && valueTokens.length > 1 && valueTokens[1] === "true")
+      {
+         direction = "ascending";
+      }
+
+      // Create a new sort widget
+      var sort = {
+         name: "alfresco/menus/AlfCheckableMenuItem",
+         config: {
+            label: msg.get(label),
+            value: value,
+            group: "DOCUMENT_LIBRARY_SORT_FIELD",
+            publishTopic: "ALF_DOCLIST_SORT_FIELD_SELECTION",
+            checked: checked,
+            publishPayload: {
+               label: msg.get(label),
+               direction: direction
+            }
+         }
+      };
+
+      // Add to the sortFields array
+      sortFields[i] = sort;
+   }
+   
+   return sortFields;
+}
+
 // Compose the sort menu
 var sortMenu = {
    id: rootWidgetId + "SORT_MENU",
@@ -102,51 +154,7 @@ var sortMenu = {
             id: "DOCLIB_SORT_FIELD_SELECT_GROUP",
             name: "alfresco/menus/AlfMenuGroup",
             config: {
-               // TODO: Add the remaining sort fields
-               widgets: [
-                  {
-                     name: "alfresco/menus/AlfCheckableMenuItem",
-                     config: {
-                        label: msg.get("faceted-search.sort-menu.type.relevance"),
-                        value: "",
-                        group: "DOCUMENT_LIBRARY_SORT_FIELD",
-                        publishTopic: "ALF_DOCLIST_SORT_FIELD_SELECTION",
-                        checked: true,
-                        publishPayload: {
-                           label: "Relevance",
-                           direction: "descending"
-                        }
-                     }
-                  },
-                  {
-                     name: "alfresco/menus/AlfCheckableMenuItem",
-                     config: {
-                        label: msg.get("faceted-search.sort-menu.type.name"),
-                        value: "cm:name",
-                        group: "DOCUMENT_LIBRARY_SORT_FIELD",
-                        publishTopic: "ALF_DOCLIST_SORT_FIELD_SELECTION",
-                        checked: false,
-                        publishPayload: {
-                           label: "Name",
-                           direction: "descending"
-                        }
-                     }
-                  },
-                  {
-                     name: "alfresco/menus/AlfCheckableMenuItem",
-                     config: {
-                        label: msg.get("faceted-search.sort-menu.type.size"),
-                        value: ".size",
-                        group: "DOCUMENT_LIBRARY_SORT_FIELD",
-                        publishTopic: "ALF_DOCLIST_SORT_FIELD_SELECTION",
-                        checked: false,
-                        publishPayload: {
-                           label: "Size",
-                           direction: "descending"
-                        }
-                     }
-                  }
-               ]
+               widgets: getSortFieldsFromConfig()
             }
          }
       ]
@@ -329,6 +337,7 @@ var searchDocLib = {
    }
 };
 
+// Put all components together
 var main = {
    name: "alfresco/layout/VerticalWidgets",
    config: {
