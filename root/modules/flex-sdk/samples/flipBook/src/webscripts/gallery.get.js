da5feed575fb7cbee594138bd05bd492edf2546d
wabson <wabson@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-06-25 16:10:29 +0000

    Merged HEAD-BUG-FIX (5.0/Cloud) to HEAD (4.3/Cloud)    73844: Merged EOL (4.3.0.EOL) to HEAD-BUG-FIX (4.3/Cloud)       73827: ACE-2015 EOL Flex SDK in 5.0          - Just remove the two projects and the flex-sdk module
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@74825 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/modules/flex-sdk/samples/flipBook/src/webscripts/gallery.get.js
+++
@@ -1,9 +0,0 @@
-
-var gallery = companyhome.childByNamePath(url.extension);
-if (gallery == undefined || gallery.isContainer == false)
-{
-	status.code = 404;
-   	status.message = "Gallery " + url.extension + " not found.";
-   	status.redirect = true;
- }
- model.gallery = gallery;
