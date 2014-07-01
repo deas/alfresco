fcd330a22c6d5078244f7b0fed105eabcd9cbc9f
wabson <wabson@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-07-01 15:40:37 +0000

    Merged HEAD-BUG-FIX (5.0/Cloud) to HEAD (5.0/Cloud)    75247: Add missing enum constants in share-po for reporting dashlets      - Constants were added as part of sync from HBF in r73204 instead of in a separate commit
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@75443 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/share-po/src/main/java/org/alfresco/po/share/enums/Dashlets.java
+++ root/projects/share-po/src/main/java/org/alfresco/po/share/enums/Dashlets.java
@@ -27,6 +27,8 @@ public enum Dashlets
     SITE_MEMBERS("Site Members"),
     SITE_PROFILE("Site Profile"),
     SITE_SEARCH("Site Search"),
+    SITE_CONTENT_REPORT("Site Content Report"),
+    TOP_SITE_CONTRIBUTOR_REPORT("Top Site Contributor Rep..."),
     WEB_VIEW("Web View"),
     WELCOME_SITE("Welcome Site"),
     WIKI("Wiki");
