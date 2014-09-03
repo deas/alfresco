4fe78ddf3c6796f836700cd26d5189d5623745bc
wabson <wabson@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-09-03 15:52:36 +0000

    Merged HEAD-BUG-FIX (5.0/Cloud) to HEAD (5.0/Cloud)    80516: Merged WAT1 (5.0/Cloud) to HEAD-BUG-FIX (5.0/Cloud)       74284: ACE-1999: Make faceted search more accessible         - Adjustment to share-po in line with accessibility changes made
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@82813 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/share-po/src/main/java/org/alfresco/po/share/search/FacetedSearchResult.java
+++ root/projects/share-po/src/main/java/org/alfresco/po/share/search/FacetedSearchResult.java
@@ -10,12 +10,12 @@ import org.openqa.selenium.WebElement;
 public class FacetedSearchResult implements SearchResult
 {
     /** Constants. */
-    private static final By NAME = By.cssSelector("tr td.nameAndTitleCell span.alfresco-renderers-Property:first-of-type span.inner a");
-    private static final By TITLE = By.cssSelector("tr td.nameAndTitleCell span.alfresco-renderers-Property:last-of-type span.value");
-    private static final By DATE = By.cssSelector("tr td.dateCell span.inner");
-    private static final By DESCRIPTION = By.cssSelector("tr td.descriptionCell span.value");
-    private static final By SITE = By.cssSelector("tr td.siteCell span.inner");
-    private static final By ACTIONS = By.cssSelector("tr td.actionsCell");
+    private static final By NAME = By.cssSelector("div.nameAndTitleCell span.alfresco-renderers-Property:first-of-type span.inner a");
+    private static final By TITLE = By.cssSelector("div.nameAndTitleCell span.alfresco-renderers-Property:last-of-type span.value");
+    private static final By DATE = By.cssSelector("div.dateCell span.inner");
+    private static final By DESCRIPTION = By.cssSelector("div.descriptionCell span.value");
+    private static final By SITE = By.cssSelector("div.siteCell span.inner");
+    private static final By ACTIONS = By.cssSelector("div.actionsCell");
 
     private WebDrone drone;
     private WebElement link;
