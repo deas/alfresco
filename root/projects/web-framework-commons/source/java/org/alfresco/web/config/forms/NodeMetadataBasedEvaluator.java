899387349f7cd6da0fcfab986a3e3871344e661a
wabson <wabson@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-09-03 18:01:38 +0000

    Merged HEAD-BUG-FIX (5.0/Cloud) to HEAD (5.0/Cloud)    80815: Fix Build (pt2) Reverse Merge HEAD-BUG-FIX (5.0/Cloud)       80357: Merged V4.2-BUG-FIX (4.2.4) to HEAD-BUG-FIX (5.0/Cloud)          79075: Merged V4.2.3 (4.2.3) to V4.2-BUG-FIX (4.2.4)             78918: Merged DEV to V4.2.3 (4.2.3)                78889: MNT-11660 : Share textarea.ftl appears even when the aspect of the property is not applied                   Was added new unit test.          79079: Merged V4.2.3 (4.2.3) to V4.2-BUG-FIX (4.2.4)             78994: Merged DEV to PATCHES/V4.2.3              78981 : MNT-11660 : Share textarea.ftl appears even when the aspect of the property is not applied                 Updated some code for unit test.          79081: Merged V4.2.3 (4.2.3) to V4.2-BUG-FIX (4.2.4)             79043: MNT-11660 : Share textarea.ftl appears even when the aspect of the property is not applied                Added webframeworkcommons classpath for unit test in build.xml .          79138: MNT-11660 : Share textarea.ftl appears even when the aspect of the property is not applied             Rename config file for test and update unit test source.          79140: MNT-11660 : Share textarea.ftl appears even when the aspect of the property is not applied             Deleted old config file for test.          79283: MNT-11660 : Share textarea.ftl appears even when the aspect of the property is not applied             Added info messages to unit test          80123: MNT-11660 : Share textarea.ftl appears even when the aspect of the property is not applied             Added info messages and log to unit test.          80207: MNT-11660 : Share textarea.ftl appears even when the aspect of the property is not applied             Updated the freemarker template for test webscript          80261: MNT-11660 : Share textarea.ftl appears even when the aspect of the property is not applied             Updated unit test.          80302: MNT-11660 : Share textarea.ftl appears even when the aspect of the property is not applied             Updated unit test.
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@83108 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/web-framework-commons/source/java/org/alfresco/web/config/forms/NodeMetadataBasedEvaluator.java
+++ root/projects/web-framework-commons/source/java/org/alfresco/web/config/forms/NodeMetadataBasedEvaluator.java
@@ -93,7 +93,7 @@ public abstract class NodeMetadataBasedEvaluator extends ServiceBasedEvaluator
         return result;
     }
 
-    protected String callMetadataService(String nodeString) throws ConnectorServiceException
+    private String callMetadataService(String nodeString) throws ConnectorServiceException
     {
         return callService("/api/metadata?nodeRef=" + nodeString + "&shortQNames=true");
     }
