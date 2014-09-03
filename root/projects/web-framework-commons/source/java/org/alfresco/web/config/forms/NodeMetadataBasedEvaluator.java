9f636eca681c47ce1aa8b59b7e562eedd04950d9
wabson <wabson@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-09-03 15:29:46 +0000

    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@82737 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/web-framework-commons/source/java/org/alfresco/web/config/forms/NodeMetadataBasedEvaluator.java
+++ root/projects/web-framework-commons/source/java/org/alfresco/web/config/forms/NodeMetadataBasedEvaluator.java
@@ -93,7 +93,7 @@ public abstract class NodeMetadataBasedEvaluator extends ServiceBasedEvaluator
         return result;
     }
 
-    private String callMetadataService(String nodeString) throws ConnectorServiceException
+    protected String callMetadataService(String nodeString) throws ConnectorServiceException
     {
         return callService("/api/metadata?nodeRef=" + nodeString + "&shortQNames=true");
     }
