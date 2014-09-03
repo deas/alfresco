34af96827a8f5f528449098c3ade24175d123ff5
wabson <wabson@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-09-03 16:02:32 +0000

    Merged HEAD-BUG-FIX (5.0/Cloud) to HEAD (5.0/Cloud)    80567: Merged WAT1 (5.0/Cloud) to HEAD-BUG-FIX (5.0/Cloud)       75710: Intern 2.0 >> Fixed AlfCheckableMenuItemTest
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@82862 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/tests/alfresco/menus/AlfCheckableMenuItemTest.js
+++ root/projects/slingshot/tests/alfresco/menus/AlfCheckableMenuItemTest.js
@@ -40,210 +40,234 @@ define(["intern!object",
             // Count the number of subscriptions to for the grouped checkable items, there should be 
             // 3 subscriptions - one for each item in the group...
             .findAllByCssSelector(TestCommon.topicSelector("ALF_CHECKABLE_MENU_ITEM__CHECKABLE_GROUP"))
-            .then(function(elements) {
-               assert(elements.length == 3, "Test #1 - A subscription for each grouped checkable menu item was expected. There should be 3 but only " + elements.length + " were found.");
-            })
+               .then(function(elements) {
+                  assert(elements.length == 3, "Test #1 - A subscription for each grouped checkable menu item was expected. There should be 3 but only " + elements.length + " were found.");
+               })
+               .end()
 
             // Test #2
             // Check that there is a subscription for checkable items with a publishTopic
             // (this is so that they can respond to external publications)
-            .hasElementByCss(TestCommon.topicSelector("CHECKABLE_1"))
-            .then(function(result) {
-               assert(result == true, "Test #2 - A subcription could not be found for a checkable menu item with a publish topic");
-            })
+            .findByCssSelector(TestCommon.topicSelector("CHECKABLE_1"))
+               .then(null, function() {
+                  assert(false, "Test #2 - A subcription could not be found for a checkable menu item with a publish topic");
+               })
+               .end()
 
             // Test #3
             // Check that there is NOT a subcription for checkable items WITHOUT a publishTopic
-            .hasElementByCss(TestCommon.topicSelector("CHECKABLE_3"))
-            .then(function(result) {
-               assert(result == false, "Test #3 - An unexpected subcription was found for a checkable menu item");
-            })
+            .findByCssSelector(TestCommon.topicSelector("CHECKABLE_3"))
+               .then(
+                  function() {
+                     // FOUND (error)
+                     assert(false, "Test #3 - An unexpected subcription was found for a checkable menu item");
+                  },
+                  function() {
+                     // DIDN'T FIND (success)
+                     assert(true, "Test #3 - Did NOT find menu item as expected");
+                  })
+               .end()
 
             // Test #4
             // Check that the first checkable item is NOT checked by default...
-            .safeEval('dijit.registry.byId("CHECKABLE_1").checked.toString()')
+            .execute('return dijit.registry.byId("CHECKABLE_1").checked.toString()')
             .then(function(result) {
                assert(result == "false", "Test #4 - The menu item was NOT checked on page load");
             })
 
             // Test #5
             // Check that the SECOND checkable item IS checked by default...
-            .safeEval('dijit.registry.byId("CHECKABLE_2").checked.toString()')
+            .execute('return dijit.registry.byId("CHECKABLE_2").checked.toString()')
             .then(function(result) {
                assert(result == "true", "Test #5 - The menu item SHOULD HAVE BEEN checked on page load");
             })
 
             // Test #6
             // Check that the first grouped checkable item IS checked and the other two aren't...
-            .safeEval('dijit.registry.byId("GROUPED_CHECKABLE_1").checked.toString()')
+            .execute('return dijit.registry.byId("GROUPED_CHECKABLE_1").checked.toString()')
             .then(function(result) {
                assert(result == "true", "Test #6 - The first GROUPED menu item SHOULD HAVE BEEN checked on page load");
             })
-            .safeEval('dijit.registry.byId("GROUPED_CHECKABLE_2").checked.toString()')
+            .execute('return dijit.registry.byId("GROUPED_CHECKABLE_2").checked.toString()')
             .then(function(result) {
                assert(result == "false", "Test #6 - The second GROUPED menu item was NOT checked on page load");
             })
-            .safeEval('dijit.registry.byId("GROUPED_CHECKABLE_3").checked.toString()')
+            .execute('return dijit.registry.byId("GROUPED_CHECKABLE_3").checked.toString()')
             .then(function(result) {
                assert(result == "false", "Test #6 - The first GROUPED menu item was NOT checked on page load");
             })
 
             // Test #7
             // Check that a checked menu item has a tick displayed
-            .hasElementByCss("tr#CHECKABLE_2>td.alf-selected-icon")
-            .then(function(result) {
-               assert(result == true, "Test #7 - A checked menu item should have the alf-selected-icon class applied to it")
-            })
+            .findByCssSelector("tr#CHECKABLE_2>td.alf-selected-icon")
+               .then(null, function() {
+                  assert(false, "Test #7 - A checked menu item should have the alf-selected-icon class applied to it")
+               })
+               .end()
 
             // Test #8
             // Check that an UNCHECKED menu item does NOT have tick displayed
-            .hasElementByCss("tr#CHECKABLE_1>td.alf-selected-icon")
-            .then(function(result) {
-               assert(result == false, "Test #8 - An UNCHECKED menu item should NOT have the alf-selected-icon class applied to it")
-            })
+            .findByCssSelector("tr#CHECKABLE_1>td.alf-selected-icon")
+               .then(
+                  function() {
+                     // FOUND == ERROR
+                     assert(false, "Test #8 - An UNCHECKED menu item should NOT have the alf-selected-icon class applied to it")
+                  },
+                  function() {
+                     // NOT FOUND == SUCCESS
+                     assert(true, "Not found as expected");
+                  })
+               .end()
 
             // Test #9
             // Check that an externally published topic can set a check box...
-            .end()
             .findByCssSelector("#SET_CHECKABLE_1_label")
                .click()
                .end()
-            .safeEval('dijit.registry.byId("CHECKABLE_1").checked.toString()')
-            .then(function(result) {
-               assert(result == "true", "Test #9 - The first menu item have been CHECKED following an external publish");
-            })
+            .execute('return dijit.registry.byId("CHECKABLE_1").checked.toString()')
+               .then(function(result) {
+                  assert(result == "true", "Test #9 - The first menu item have been CHECKED following an external publish");
+               })
+               .end()
 
             // Test #10
             // Check that an externally published topic can unset a check box...
-            .end()
             .findByCssSelector("#UNSET_CHECKABLE_1_label")
                .click()
                .end()
-            .safeEval('dijit.registry.byId("CHECKABLE_1").checked.toString()')
-            .then(function(result) {
-               assert(result == "false", "Test #10 - The first menu item have been UNCHECKED following an external publish");
-            })
+            .execute('return dijit.registry.byId("CHECKABLE_1").checked.toString()')
+               .then(function(result) {
+                  assert(result == "false", "Test #10 - The first menu item have been UNCHECKED following an external publish");
+               })
+               .end()
 
             // Test #11
             // Check that an externally published topic updates a group accordingly...
-            .end()
             .findByCssSelector("#SET_GROUPED_CHECKABLE_2_label")
                .click()
                .end()
-            .safeEval('dijit.registry.byId("GROUPED_CHECKABLE_1").checked.toString()')
-            .then(function(result) {
-               assert(result == "false", "Test #11 - The first GROUPED menu item have been UNCHECKED following an external publish");
-            })
-            .safeEval('dijit.registry.byId("GROUPED_CHECKABLE_2").checked.toString()')
-            .then(function(result) {
-               assert(result == "true", "Test #11 - The second GROUPED menu item have been CHECKED following an external publish");
-            })
+            .execute('return dijit.registry.byId("GROUPED_CHECKABLE_1").checked.toString()')
+               .then(function(result) {
+                  assert(result == "false", "Test #11 - The first GROUPED menu item have been UNCHECKED following an external publish");
+               })
+               .end()
+            .execute('return dijit.registry.byId("GROUPED_CHECKABLE_2").checked.toString()')
+               .then(function(result) {
+                  assert(result == "true", "Test #11 - The second GROUPED menu item have been CHECKED following an external publish");
+               })
+               .end()
 
             // Test #12
             // Use mouse navigation to drive the checkable menu items...
-            .end()
             .findByCssSelector("#CHECKABLE_MENU_ITEMS_DROPDOWN")
                .click()
                .end()
             .findByCssSelector("#CHECKABLE_1")
                .click()
                .end()
-            .hasElementByCss(TestCommon.topicSelector("CHECKABLE_1", "publish", "last"))
-            .then(function(result) {
-               assert(result == true, "Test #12 - Mouse selection of CHECKABLE_1 didn't publish correctly (missing publish topic)");
-            })
-            .end()
-            .hasElementByCss(TestCommon.pubSubDataCssSelector("last", "selected", "true"))
-            .then(function(result) {
-               assert(result == true, "Test #12 - Mouse selection of CHECKABLE_1 didn't publish correctly (incorrect 'selected' payload attribute");
-            })
-            .end()
+            .findByCssSelector(TestCommon.topicSelector("CHECKABLE_1", "publish", "last"))
+               .then(null, function() {
+                  assert(false, "Test #12 - Mouse selection of CHECKABLE_1 didn't publish correctly (missing publish topic)");
+               })
+               .end()
+            .findByCssSelector(TestCommon.pubSubDataCssSelector("last", "selected", "true"))
+               .then(null, function() {
+                  assert(false, "Test #12 - Mouse selection of CHECKABLE_1 didn't publish correctly (incorrect 'selected' payload attribute");
+               })
+               .end()
             .findByCssSelector("#CHECKABLE_MENU_ITEMS_DROPDOWN")
                .click()
                .end()
             .findByCssSelector("#CHECKABLE_1")
                .click()
                .end()
-            .hasElementByCss(TestCommon.pubSubDataCssSelector("last", "selected", "false"))
-            .then(function(result) {
-               assert(result == true, "Test #12 - Mouse de-selection of CHECKABLE_1 didn't publish correctly (incorrect 'selected' payload attribute");
-            })
+            .findByCssSelector(TestCommon.pubSubDataCssSelector("last", "selected", "false"))
+               .then(null, function() {
+                  assert(false, "Test #12 - Mouse de-selection of CHECKABLE_1 didn't publish correctly (incorrect 'selected' payload attribute");
+               })
+               .end()
 
             // Test #13
             // Use keyboard navigation to drive the checkable menu items...
-            .end()
             .pressKeys(keys.ARROW_DOWN)
             .pressKeys(keys.ARROW_DOWN)
             .sleep(1000)
             .pressKeys(keys.SPACE)
-            .hasElementByCss(TestCommon.topicSelector("CHECKABLE_2", "publish", "last"))
-            .then(function(result) {
-               assert(result == true, "Test #13 - Keyboard selection of CHECKABLE_2 didn't publish correctly");
-            })
-            .end()
-            .hasElementByCss(TestCommon.pubSubDataCssSelector("last", "selected", "false"))
-            .then(function(result) {
-               assert(result == true, "Test #13 - Keyboard selection of CHECKABLE_2 didn't publish correctly (incorrect 'selected' payload attribute");
-            })
+            .findByCssSelector(TestCommon.topicSelector("CHECKABLE_2", "publish", "last"))
+               .then(null, function() {
+                  assert(false, "Test #13 - Keyboard selection of CHECKABLE_2 didn't publish correctly");
+               })
+               .end()
+            .findByCssSelector(TestCommon.pubSubDataCssSelector("last", "selected", "false"))
+               .then(null, function() {
+                  assert(false, "Test #13 - Keyboard selection of CHECKABLE_2 didn't publish correctly (incorrect 'selected' payload attribute");
+               })
+               .end()
+            
             // The second checkable item has some additional data in the payload, check that it has also been published...
-            .end()
-            .hasElementByCss(TestCommon.pubSubDataCssSelector("last", "clicked", "CHECKABLE_2"))
-            .then(function(result) {
-               assert(result == true, "Test #13 - Keyboard selection of CHECKABLE_2 didn't publish additional payload attribute");
-            })
+            .findByCssSelector(TestCommon.pubSubDataCssSelector("last", "clicked", "CHECKABLE_2"))
+               .then(null, function() {
+                  assert(false, "Test #13 - Keyboard selection of CHECKABLE_2 didn't publish additional payload attribute");
+               })
+               .end()
             
             // Test #14
             // Use keyboard navigation to drive the grouped checkable menu items...
-            .end()
             .pressKeys(keys.ARROW_DOWN)
             .pressKeys(keys.ARROW_DOWN)
             .pressKeys(keys.ARROW_DOWN)
             .pressKeys(keys.ARROW_DOWN)
             .sleep(1000)
             .pressKeys(keys.RETURN)
-            .hasElementByCss(TestCommon.topicSelector("ALF_CHECKABLE_MENU_ITEM__CHECKABLE_GROUP", "publish"))
-            .then(function(result) {
-               assert(result == true, "Test #14 - Keyboard selection of grouped item didn't publish to group");
-            })
-            .safeEval('dijit.registry.byId("GROUPED_CHECKABLE_1").checked.toString()')
-            .then(function(result) {
-               assert(result == "true", "Test #14 - The first GROUPED menu item should have been CHECKED following keyboard navigation");
-            })
-            .safeEval('dijit.registry.byId("GROUPED_CHECKABLE_2").checked.toString()')
-            .then(function(result) {
-               assert(result == "false", "Test #14 - The second GROUPED menu item should have been UNCHECKED following keyboard navigation");
-            })
-            .safeEval('dijit.registry.byId("GROUPED_CHECKABLE_3").checked.toString()')
-            .then(function(result) {
-               assert(result == "false", "Test #14 - The third GROUPED menu item should have been UNCHECKED following keyboard navigation");
-            })
+            .findByCssSelector(TestCommon.topicSelector("ALF_CHECKABLE_MENU_ITEM__CHECKABLE_GROUP", "publish"))
+               .then(null, function() {
+                  assert(false, "Test #14 - Keyboard selection of grouped item didn't publish to group");
+               })
+               .end()
+            .execute('return dijit.registry.byId("GROUPED_CHECKABLE_1").checked.toString()')
+               .then(function(result) {
+                  assert(result == "true", "Test #14 - The first GROUPED menu item should have been CHECKED following keyboard navigation");
+               })
+               .end()
+            .execute('return dijit.registry.byId("GROUPED_CHECKABLE_2").checked.toString()')
+               .then(function(result) {
+                  assert(result == "false", "Test #14 - The second GROUPED menu item should have been UNCHECKED following keyboard navigation");
+               })
+               .end()
+            .execute('return dijit.registry.byId("GROUPED_CHECKABLE_3").checked.toString()')
+               .then(function(result) {
+                  assert(result == "false", "Test #14 - The third GROUPED menu item should have been UNCHECKED following keyboard navigation");
+               })
+               .end()
 
             // Test #15
             // Use mouse navigation to drive the grouped checkable menu items...
-            .end()
             .findByCssSelector("#CHECKABLE_MENU_ITEMS_DROPDOWN")
                .click()
                .end()
             .findByCssSelector("#GROUPED_CHECKABLE_3")
                .click()
                .end()
-            .hasElementByCss(TestCommon.topicSelector("ALF_CHECKABLE_MENU_ITEM__CHECKABLE_GROUP", "publish"))
-            .then(function(result) {
-               assert(result == true, "Test #15 - Mouse selection of grouped item didn't publish to group");
-            })
-            .end()
-            .safeEval('dijit.registry.byId("GROUPED_CHECKABLE_1").checked.toString()')
-            .then(function(result) {
-               assert(result == "false", "Test #15 - The first GROUPED menu item should have been UNCHECKED following mouse navigation");
-            })
-            .safeEval('dijit.registry.byId("GROUPED_CHECKABLE_2").checked.toString()')
-            .then(function(result) {
-               assert(result == "false", "Test #15 - The second GROUPED menu item should have been UNCHECKED following mouse navigation");
-            })
-            .safeEval('dijit.registry.byId("GROUPED_CHECKABLE_3").checked.toString()')
-            .then(function(result) {
-               assert(result == "true", "Test #15 - The third GROUPED menu item should have been CHECKED following mouse navigation");
-            })
+            .findByCssSelector(TestCommon.topicSelector("ALF_CHECKABLE_MENU_ITEM__CHECKABLE_GROUP", "publish"))
+               .then(null, function() {
+                  assert(false, "Test #15 - Mouse selection of grouped item didn't publish to group");
+               })
+               .end()
+            .execute('return dijit.registry.byId("GROUPED_CHECKABLE_1").checked.toString()')
+               .then(function(result) {
+                  assert(result == "false", "Test #15 - The first GROUPED menu item should have been UNCHECKED following mouse navigation");
+               })
+               .end()
+            .execute('return dijit.registry.byId("GROUPED_CHECKABLE_2").checked.toString()')
+               .then(function(result) {
+                  assert(result == "false", "Test #15 - The second GROUPED menu item should have been UNCHECKED following mouse navigation");
+               })
+               .end()
+            .execute('return dijit.registry.byId("GROUPED_CHECKABLE_3").checked.toString()')
+               .then(function(result) {
+                  assert(result == "true", "Test #15 - The third GROUPED menu item should have been CHECKED following mouse navigation");
+               })
+               .end()
 
             // Post the coverage results...
             .then(function() {
