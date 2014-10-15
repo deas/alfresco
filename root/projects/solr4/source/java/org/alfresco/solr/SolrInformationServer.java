c826b435f555a323d5b3a66c030debbe523ac7f8
ahind <ahind@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-10-15 20:08:41 +0000

    Fix for     ACE-3136    Site Search dashlet on Site Dashboard doesn't work for Site whose name and URL begin with a digit
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@88194 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/solr4/source/java/org/alfresco/solr/SolrInformationServer.java
+++ root/projects/solr4/source/java/org/alfresco/solr/SolrInformationServer.java
@@ -1774,7 +1774,7 @@ public class SolrInformationServer implements InformationServer
             if(matcher.find())
             {
                 repoOnly = false;
-                doc.addField(FIELD_SITE, matcher.group(1));
+                doc.addField(FIELD_SITE, ISO9075.decode(matcher.group(1)));
             }
             
             matcher = CAPTURE_SHARED_FILES.matcher(path.getFirst());
@@ -1787,7 +1787,7 @@ public class SolrInformationServer implements InformationServer
             matcher = CAPTURE_TAG.matcher(path.getFirst());
             if(matcher.find())
             {
-                String tag = matcher.group(1);
+                String tag = ISO9075.decode(matcher.group(1));
                 doc.addField(FIELD_TAG, tag);
                 doc.addField(FIELD_SUGGEST, tag);
             }
