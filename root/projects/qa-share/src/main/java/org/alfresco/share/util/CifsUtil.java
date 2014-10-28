85cedf1d759635eaa4988c90cca1fdcaae49da4b
snagarajan <snagarajan@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-10-28 12:23:36 +0000

    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@89298 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/qa-share/src/main/java/org/alfresco/share/util/CifsUtil.java
+++ root/projects/qa-share/src/main/java/org/alfresco/share/util/CifsUtil.java
@@ -1,9 +1,16 @@
 package org.alfresco.share.util;
 
 import jcifs.smb.*;
+
 import org.alfresco.po.share.util.PageUtils;
 import org.testng.Assert;
 
+import java.awt.Image;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.*;
 import java.util.Arrays;
 import java.util.List;
@@ -14,9 +21,11 @@ import static org.testng.Assert.assertTrue;
 /**
  * @author Olga Antonik
  */
-public class CifsUtil extends AbstractUtils
+public class CifsUtil extends AbstractUtils implements Transferable, ClipboardOwner
 {
 
+    private Image image;
+    
     /**
      * Method to add document to the Alfresco via CIFS
      * 
@@ -495,5 +504,53 @@ public class CifsUtil extends AbstractUtils
             successful = false;
         }
         return successful;
+    }
+    
+    public CifsUtil(Image image)
+    {
+        this.image = image;
+    }
+
+    public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException
+    {
+
+        if (flavor.equals(DataFlavor.imageFlavor) && image != null)
+        {
+            return image;
+        }
+        else
+        {
+            throw new UnsupportedFlavorException(flavor);
+        }
+    }
+
+    @Override
+    public DataFlavor[] getTransferDataFlavors()
+    {
+        DataFlavor[] flavors = new DataFlavor[1];
+        flavors[0] = DataFlavor.imageFlavor;
+        return flavors;
+    }
+
+    @Override
+    public boolean isDataFlavorSupported(DataFlavor flavor)
+    {
+        DataFlavor[] flavors = getTransferDataFlavors();
+        for (int i = 0; i < flavors.length; i++)
+        {
+            if (flavor.equals(flavors[i]))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void lostOwnership(Clipboard arg0, Transferable arg1)
+    {
+        // TODO Auto-generated method stub
+
     }
 }
