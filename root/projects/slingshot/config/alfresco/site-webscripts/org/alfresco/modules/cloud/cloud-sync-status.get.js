32402e7d364ab08305cf1157a345f9ab4e4bcc56
dwebster <dwebster@7033f55c-6c29-4ad8-afba-765cb616e738>
2014-10-30 16:51:18 +0000

    ACE-3130: I've refactored the error handling in the JS controller to make it more readable and the code no longer sets synced=false when receiving a 403 response: I can't think of a valid error case where a synced node would receive a 403 if the node is not synced.
    git-svn-id: http://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@89571 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/modules/cloud/cloud-sync-status.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/modules/cloud/cloud-sync-status.get.js
@@ -1,5 +1,5 @@
-<import resource="classpath:alfresco/site-webscripts/org/alfresco/config.lib.js">
-<import resource="classpath:/alfresco/templates/org/alfresco/import/alfresco-util.js">
+//<import resource="classpath:alfresco/site-webscripts/org/alfresco/config.lib.js">
+//<import resource="classpath:/alfresco/templates/org/alfresco/import/alfresco-util.js">
 
 function main()
 {
@@ -9,9 +9,12 @@ function main()
    model.synced = true;
 
    model.syncMode = syncMode.value;
+
+   // Get the nodeRef for the remote node.
    var remoteNodeInfo = AlfrescoUtil.getRemoteNodeRef(model.nodeRef),
       nodeDetails = null;
 
+   // If we've got the remote nodeRef, let's fetch the details for it, otherwise lookup parent details (it might be indirectly synced).
    if (remoteNodeInfo && remoteNodeInfo.remoteNodeRef)
    {
       nodeDetails = AlfrescoUtil.getRemoteNodeDetails(remoteNodeInfo.remoteNodeRef, remoteNodeInfo.remoteNetworkId);
@@ -21,47 +24,60 @@ function main()
       model.isParentPath = true;
       nodeDetails = AlfrescoUtil.getRemoteNodeDetails(remoteNodeInfo.remoteParentNodeRef, remoteNodeInfo.remoteNetworkId);
    }
-   
-   if (nodeDetails && nodeDetails.error && nodeDetails.error.status)
-   {
-      model.error = {
-         code: nodeDetails.error.status.code, 
-         message: nodeDetails.error.message};
-   }
 
-   if (nodeDetails && nodeDetails.error && nodeDetails.error.status && nodeDetails.error.status.code === 403)
+   if (nodeDetails)
    {
-      // 403 returned when not is not a sync set member.
-      model.synced = false;
-      model.error.message = msg.get("sync.status.unknown-location.unauthorized");
-   }
-   else if (nodeDetails && nodeDetails.error && nodeDetails.error.status && nodeDetails.error.status.code === 410)
-   {
-      // 410 returned if there is no permissions to get node details
-      model.error.message = msg.get("sync.status.unknown-location.no-permissions");
-   }
-   // there is no error here but we unable to build node path
-   else if (nodeDetails && !nodeDetails.error && !nodeDetails.item.location.site)
-   {
-      model.error = {
-         code: 410, 
-         message: msg.get("sync.status.unknown-location.no-permissions")};
-   }
-   else if (nodeDetails && !nodeDetails.error)
-   {
-      model.nodeFound = true;
-      model.item = nodeDetails.item;
-      model.node = nodeDetails.item.node;
-      model.paths = AlfrescoUtil.getPaths(nodeDetails, model.rootPage, model.rootLabelId);
-      model.site = nodeDetails.item.location.site;
-      model.remoteNetworkId = remoteNodeInfo.remoteNetworkId;
-      model.nodeTitle = nodeDetails.item.node.properties["cm:name"];
-      model.shareURL = nodeDetails.metadata.shareURL;
-      model.isContainer = nodeDetails.item.node.isContainer;
-      model.isDirectSync = nodeDetails.item.node.properties["sync:directSync"];
-      model.rootNodeRef = remoteNodeInfo.localRootNodeRef;
-      model.rootNodeName = remoteNodeInfo.localRootNodeName;
-      model.syncOwnerFullName = (remoteNodeInfo.syncSetOwnerFirstName + " " + remoteNodeInfo.syncSetOwnerLastName).replace(/^\s+|\s+$/g, "");
+      if (nodeDetails.error)
+      {
+         // Pass through the error by default:
+         model.error = {
+            code: nodeDetails.error.status.code,
+            message: nodeDetails.error.message
+         };
+         // Handle special errors
+         switch (nodeDetails.error.status.code) {
+            case 403:
+               // On premise permissions error.
+               // 403 returned when user doesn't have permission request node details (e.g. no cloud credentials)
+               model.error.message = msg.get("sync.status.unknown-location.unauthorized");
+               break;
+            case 410:
+               // Remote user permissions error.
+               // 410 returned if the user is authenticated to remote repo, but remote user lacks node permissions to get details.
+               model.error.message = msg.get("sync.status.unknown-location.no-permissions");
+               break;
+         }
+      }
+      // There's no error:
+      else
+      {
+         // Even though there's no error, lets check we've got the essential location.
+         // TODO: I'm not sure when this would occur.
+         if (!nodeDetails.item.location.site)
+         {
+            model.error = {
+               code: 410,
+               message: msg.get("sync.status.unknown-location.no-permissions")
+            };
+         }
+         // Everything went well:
+         else
+         {
+            model.nodeFound = true;
+            model.item = nodeDetails.item;
+            model.node = nodeDetails.item.node;
+            model.paths = AlfrescoUtil.getPaths(nodeDetails, model.rootPage, model.rootLabelId);
+            model.site = nodeDetails.item.location.site;
+            model.remoteNetworkId = remoteNodeInfo.remoteNetworkId;
+            model.nodeTitle = nodeDetails.item.node.properties["cm:name"];
+            model.shareURL = nodeDetails.metadata.shareURL;
+            model.isContainer = nodeDetails.item.node.isContainer;
+            model.isDirectSync = nodeDetails.item.node.properties["sync:directSync"];
+            model.rootNodeRef = remoteNodeInfo.localRootNodeRef;
+            model.rootNodeName = remoteNodeInfo.localRootNodeName;
+            model.syncOwnerFullName = (remoteNodeInfo.syncSetOwnerFirstName + " " + remoteNodeInfo.syncSetOwnerLastName).replace(/^\s+|\s+$/g, "");
+         }
+      }
    }
 }
 
